bba2c5fb14c41ea7069f70392e0eb6ef7d4dc009
* <lucasduartedeveloper@gmail.com>
2021-12-20 19:45:44 -0300

    first commit.

--- index.php
+++ index.php
@@ -100,6 +100,7 @@ function initMap() {
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
 
 <script
       src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap&v=weekly" 
