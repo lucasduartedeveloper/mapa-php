1771a2b806b2ccaedb266afc9f7e7ca69c4684fa
* <lucasduartedeveloper@gmail.com>
2021-12-20 19:59:43 -0300

    first commit.

--- index.php
+++ index.php
@@ -97,6 +97,9 @@ function initMap() {
     </button>
 </div>
 
+<script src="//cdn.jsdelivr.net/npm/eruda"></script>
+<script>eruda.init();</script>
+
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
