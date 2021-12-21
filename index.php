01d23160e176e1be64a74bca0e0f3fa522e82bfd
* <lucasduartedeveloper@gmail.com>
2021-12-20 23:37:50 -0300

    first commit.

--- index.php
+++ index.php
@@ -38,47 +38,6 @@ catch (PDOException $e) {
 <script src="https://viajem.herokuapp.com/script.js"></script>
 <script src="https://viajem.herokuapp.com/geolocation.js"></script>
 
-<script>
-// Set the date we're counting down to
-var countDownDate = new Date("Dec 23, 2021 20:55:00").getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get today's date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the resultghp
-  document.getElementById("dias").innerHTML = days + "dias " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-
-  document.getElementById("horas").innerHTML = (days * 24) + hours + "h "
-  + minutes + "m " + seconds + "s ";
-
-  document.getElementById("minutos").innerHTML = (((days * 24) + hours) * 60)
- + minutes + "m " + seconds + "s ";
-
-  document.getElementById("segundos").innerHTML = (((((days * 24) + hours) * 60) + minutes) * 60) + seconds + "s ";
-
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("dias").innerHTML = "EXPIRED";
-    document.getElementById("horas").innerHTML = "EXPIRED";
-    document.getElementById("minutos").innerHTML = "EXPIRED";
-    document.getElementById("segundos").innerHTML = "EXPIRED";
-  }
-}, 1000);
-</script>
 </head>
 <body>
 
