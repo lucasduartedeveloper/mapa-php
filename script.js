4f6b623da7352f1618b450d370cf098191044e77
lucasduartedeveloper@gmail.com <lucasduartedeveloper@gmail.com>
2021-12-22 20:42:27 -0300

    first commit

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ var x = setInterval(function() {
 $(document).ready(function() {
   console.log("document.ready");
   $.getJSON( "/ajax/localizacao.php?lat=0&long=0", function(data) {
-       map.setView([data.latitude, data.longitude], 18);
+       map.setView([data.latitude, data.longitude], 17);
        marker.setLatLng(new L.LatLng(data.latitude, data.longitude));
        circle.setLatLng(new L.LatLng(data.latitude, data.longitude));
        rectangle.setBounds(circle.getBounds());
@@ -52,9 +52,9 @@ $(document).ready(function() {
 
   $( "#target" ).click(function() {
     $.getJSON( "/ajax/localizacao.php?lat="+geolocation.latitude+"&long="+geolocation.longitude, function(data) {
-       map.setView([data.latitude, data.longitude], 18);
+       map.setView([data.latitude, data.longitude], 17);
        marker.setLatLng(new L.LatLng(data.latitude, data.longitude));
-       cicle.setLatLng(new L.LatLng(data.latitude, data.longitude));
+       circle.setLatLng(new L.LatLng(data.latitude, data.longitude));
        rectangle.setBounds(circle.getBounds());
        rectangle.redraw();
        console.log(JSON.stringify(data));
