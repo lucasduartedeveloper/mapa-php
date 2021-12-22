d0418d6e9bcad5b5e9299dedca075670103cf661
* <lucasduartedeveloper@gmail.com>
2021-12-22 13:13:12 -0300

    first commit.

--- index.php
+++ index.php
@@ -26,37 +26,6 @@ catch (PDOException $e) {
 <html>
 <head>
 
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"/>
-<link rel="stylesheet" href="https://viajem.herokuapp.com/css/style.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<title></title>
-
- <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
-
-
-</head>
-<body>
-
-<div id="map" class="box">
-</div>
-<div class="box2">
-   <p class="contador"><?=$contar?> visitas</p>
-    <p id="dias"></p>
-    <p id="horas"></p>
-    <p id="minutos"></p>
-    <p id="segundos"></p>
-    <button id="target" type="button" class="btn btn-outline-dark">
-    <i class="bi bi-geo-alt"></i>
-    Salvar
-    </button>
-</div>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-<script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
 
 <script>
 console.log(",script.js");
@@ -108,7 +77,40 @@ $(document).ready(function()
   })
 );
 </script>
-<script src="https://viajem.herokuapp.com/script.js"></script>
+
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"/>
+<link rel="stylesheet" href="https://viajem.herokuapp.com/css/style.css?v=0">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title></title>
+
+ <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+
+
+</head>
+<body>
+
+<div id="map" class="box">
+</div>
+<div class="box2">
+   <p class="contador"><?=$contar?> visitas</p>
+    <p id="dias"></p>
+    <p id="horas"></p>
+    <p id="minutos"></p>
+    <p id="segundos"></p>
+    <button id="target" type="button" class="btn btn-outline-dark btn-sm">
+    <i class="bi bi-geo-alt"></i>
+    Salvar
+    </button>
+</div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+
+<script src="https://viajem.herokuapp.com/script.js?v=0"></script>
 <script src="https://viajem.herokuapp.com/geolocation.js?v=0"></script>
 
 <script src="//cdn.jsdelivr.net/npm/eruda"></script>
