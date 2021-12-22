d81acb5271912e4b87fc4fa1678e19a2b81ec9ae
* <lucasduartedeveloper@gmail.com>
2021-12-22 14:25:52 -0300

    first commit.

--- ajax/localizacao.php
+++ ajax/localizacao.php
@@ -1,14 +1,14 @@
 <?php include ('../config/db.php')?>
 <?php
 try {
-  $sql = "SELECT valor FROM param WHERE nome='contador_visitas';";
-  //echo $sql;
+  $sql = "SELECT latitude,longitude FROM localizacao ORDER BY id DESC";
+  echo $sql;
   $stmt = $pdo->prepare($sql);
   $stmt->execute();
   $rowCount = $stmt->rowCount();
   $details = $stmt->fetch(); 
-  $contar = intval($details->valor);
-  echo $contar;
+  //$contar = intval($details->valor);
+  //echo $contar;
 }
 catch (PDOException $e) {
    echo 'Connection failed: ' . $e->getMessage();
