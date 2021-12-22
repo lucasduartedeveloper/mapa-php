d0008676f279d390aad0dd7998d5dd98e8bb8409
lucasduartedeveloper@gmail.com <lucasduartedeveloper@gmail.com>
2021-12-22 15:30:02 -0300

    first commit.

--- ajax/localizacao.php
+++ ajax/localizacao.php
@@ -3,14 +3,16 @@
 try {
   $latitude = htmlspecialchars($_GET["lat"]);
   $longitude = htmlspecialchars($_GET["long"]);
-
-  $sql = "INSERT INTO localizacao(latitude,longitude) VALUES ('".$latitude."','".$longitude."');";
-   echo $sql."<br>";
-  $stmt = $pdo->prepare($sql);
-  $stmt->execute();
+  
+  If (!empty($latitude)) {
+    $sql = "INSERT INTO localizacao(latitude,longitude) VALUES ('".$latitude."','".$longitude."');";
+     //echo $sql."<br>";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+  }
 
   $sql = "SELECT latitude,longitude,data_hora FROM localizacao ORDER BY id DESC LIMIT 1;";
-  echo $sql."<br>";
+  //echo $sql."<br>";
   $stmt = $pdo->prepare($sql);
   $stmt->execute();
   $rowCount = $stmt->rowCount();
