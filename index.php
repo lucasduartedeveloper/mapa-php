18dc61be043e1f4bd05251713496626d2d55dee3
* <lucasduartedeveloper@gmail.com>
2021-12-22 12:55:40 -0300

    first commit.

--- index.php
+++ index.php
@@ -40,14 +40,16 @@ catch (PDOException $e) {
 <body>
 
 <div id="map" class="box">
+</div>
+<div class="box2">
    <p class="contador"><?=$contar?> visitas</p>
     <p id="dias"></p>
     <p id="horas"></p>
     <p id="minutos"></p>
     <p id="segundos"></p>
     <button id="target" type="button" class="btn btn-outline-dark">
-    <i class="bi bi-unlock-fill"></i>
-    Abrir
+    <i class="bi bi-geo-alt"></i>
+    Salvar
     </button>
 </div>
 
@@ -56,6 +58,56 @@ catch (PDOException $e) {
 <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
 
+<script>
+console.log(",script.js");
+
+// Set the date we're counting down to
+var countDownDate = new Date("Dec 23, 2021 20:55:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the resultghp
+  document.getElementById("dias").innerHTML = days + "dias " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  document.getElementById("horas").innerHTML = (days * 24) + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  document.getElementById("minutos").innerHTML = (((days * 24) + hours) * 60)
+ + minutes + "m " + seconds + "s ";
+
+  document.getElementById("segundos").innerHTML = (((((days * 24) + hours) * 60) + minutes) * 60) + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("dias").innerHTML = "EXPIRED";
+    document.getElementById("horas").innerHTML = "EXPIRED";
+    document.getElementById("minutos").innerHTML = "EXPIRED";
+    document.getElementById("segundos").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+$(document).ready(function()
+  console.log("document.ready");
+  $( "#target" ).click(function() {
+    alert( "Handler for .click() called." );
+  })
+);
+</script>
 <script src="https://viajem.herokuapp.com/script.js"></script>
 <script src="https://viajem.herokuapp.com/geolocation.js?v=0"></script>
 
