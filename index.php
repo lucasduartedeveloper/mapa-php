bca9ae13916932e794aaa5d52a3c61d409a56424
lucasduartedeveloper@gmail.com <lucasduartedeveloper@gmail.com>
2021-12-22 21:12:31 -0300

    first commit

--- index.php
+++ index.php
@@ -61,7 +61,7 @@ catch (PDOException $e) {
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 
 <script src="https://viajem.herokuapp.com/script.js?v=21"></script>
-<script src="https://viajem.herokuapp.com/geolocation.js?v=12"></script>
+<script src="https://viajem.herokuapp.com/geolocation.js?v=13"></script>
 
 <script src="//cdn.jsdelivr.net/npm/eruda"></script>
 <script>eruda.init();</script>
