b48c2bea938fe75bbee481d69d99febb9d56b25c
lucasduartedeveloper@gmail.com <lucasduartedeveloper@gmail.com>
2022-01-06 10:35:01 -0300

    first commit

--- geolocation.js
+++ geolocation.js
@@ -30,9 +30,9 @@ var greenIcon = L.icon({
     shadowUrl: '/img/monster-shadow.png',
 
     iconSize:     [35, 50], // size of the icon
-    shadowSize:   [35, 25], // size of the shadow
+    shadowSize:   [50, 25], // size of the shadow
     iconAnchor:   [17.5, 50], // point of the icon which will correspond to marker's location
-    shadowAnchor: [17.5, 55],  // the same for the shadow
+    shadowAnchor: [17.5, -5],  // the same for the shadow
     popupAnchor:  [17.5, 50] // point from which the popup should open relative to the iconAnchor
 });
 
