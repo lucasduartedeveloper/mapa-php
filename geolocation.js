96b2b9044e3a436e938566945d43b7fb55977a97
lucasduartedeveloper@gmail.com <lucasduartedeveloper@gmail.com>
2022-01-06 01:17:01 -0300

    first commit

--- geolocation.js
+++ geolocation.js
@@ -27,10 +27,10 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 
 var greenIcon = L.icon({
     iconUrl: '/img/monster.png',
-    shadowUrl: '/img/monster-shadow.png',
+    shadowUrl: '/img/monster-shadow.jpe',
 
-    iconSize:     [38, 95], // size of the icon
-    shadowSize:   [50, 64], // size of the shadow
+    iconSize:     [35, 50], // size of the icon
+    shadowSize:   [35, 50], // size of the shadow
     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
     shadowAnchor: [4, 62],  // the same for the shadow
     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
