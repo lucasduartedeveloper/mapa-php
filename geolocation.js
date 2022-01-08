1f3e1d93339d55bf7a50eb39ad497e7aa7d97e11
lucasduartedeveloper@gmail.com <lucasduartedeveloper@gmail.com>
2022-01-08 20:28:25 -0300

    first commit

--- geolocation.js
+++ geolocation.js
@@ -18,7 +18,7 @@ var marker = L.marker([51.505, -0.09]).addTo(map);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
+    maxZoom: 20,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
--- index.php
+++ index.php
@@ -48,7 +48,7 @@ catch (PDOException $e) {
 <div id="map" class="box">
 </div>
 <div class="box2">
-    <p class="local">INVENTÁRIO</p>
+    <p class="local">COISAS VIRTUAIS</p>
     <div id="teste1" class="btn-group btn-group-toggle" data-toggle="buttons">
     </div>
     <div id="teste2" class="btn-group btn-group-toggle" data-toggle="buttons">
@@ -77,8 +77,8 @@ catch (PDOException $e) {
 <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 
-<script src="/script.js?v=71"></script>
-<script src="/geolocation.js?v=45"></script> 
+<script src="/script.js?v=73"></script>
+<script src="/geolocation.js?v=46"></script> 
 
 <script src="//cdn.jsdelivr.net/npm/eruda"></script>
 <script>eruda.init();</script>
