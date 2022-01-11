59cc2709dd60be87f3f733713556cf57792f77f5
lucasduartedeveloper@gmail.com <lucasduartedeveloper@gmail.com>
2022-01-11 20:57:17 -0300

    first commit

--- script.js
+++ script.js
@@ -130,45 +130,46 @@ function saveImage() {
 
             var reader = new FileReader();
             // Set the image once loaded into file reader
-            reader.onloadend = function(e) {
+            reader.onload = function(e) {
                 var img = document.createElement("img");
-                img.src = e.target.result;
+                img.onload = function () {
+                    var canvas = document.createElement("canvas");
+                    var ctx = canvas.getContext("2d");
+                    ctx.drawImage(img, 0, 0);
 
-                var canvas = document.createElement("canvas");
-                var ctx = canvas.getContext("2d");
-                ctx.drawImage(img, 0, 0);
+                    var MAX_WIDTH = 200;
+                    var MAX_HEIGHT = 200;
+                    var width = img.width;
+                    var height = img.height;
 
-                var MAX_WIDTH = 200;
-                var MAX_HEIGHT = 200;
-                var width = img.width;
-                var height = img.height;
-
-                if (width > height) {
-                    if (width > MAX_WIDTH) {
-                        height *= MAX_WIDTH / width;
-                        width = MAX_WIDTH;
-                    }
-                } else {
+                    if (width > height) {
+                        if (width > MAX_WIDTH) {
+                            height *= MAX_WIDTH / width;
+                            width = MAX_WIDTH;
+                        }
+                    } else {
                     if (height > MAX_HEIGHT) {
-                        width *= MAX_HEIGHT / height;
-                        height = MAX_HEIGHT;
+                            width *= MAX_HEIGHT / height;
+                            height = MAX_HEIGHT;
+                        }
                     }
+
+                    canvas.width = width;
+                    canvas.height = height;
+                    var ctx = canvas.getContext("2d");
+                    ctx.drawImage(img, 0, 0, width, height);
+
+                    document
+                   .getElementById('output')
+                   .parentNode
+                   .insertBefore(canvas,
+                   document.getElementById('output'));
+
+                   dataurl = canvas.toDataURL(file.type);
+                   console.log(dataurl);
+                   document.getElementById('output').src = dataurl;
                 }
-
-                canvas.width = width;
-                canvas.height = height;
-                var ctx = canvas.getContext("2d");
-                ctx.drawImage(img, 0, 0, width, height);
-
-                document
-                .getElementById('output')
-                .parentNode
-                .insertBefore(canvas,
-                document.getElementById('output'));
-
-                dataurl = canvas.toDataURL(file.type);
-                console.log(dataurl);
-                document.getElementById('output').src = dataurl;
+                img.src = e.target.result;
             }
             reader.readAsDataURL(file);
         }
