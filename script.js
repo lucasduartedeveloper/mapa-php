b32e498812f4cff9de34f8804068d41279b517a1
lucasduartedeveloper@gmail.com <lucasduartedeveloper@gmail.com>
2022-01-11 17:45:59 -0300

    first commit

--- script.js
+++ script.js
@@ -143,7 +143,7 @@ $('#add').click(function() {
    $('#camera').click();
 });
 
-function() {
+function salvarImagem() {
   //var preview = document.querySelector('img');
   var file = document.querySelector('input[type=file]').files[0];
   var reader  = new FileReader();
