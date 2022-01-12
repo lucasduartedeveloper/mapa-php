6074cf8266ae56f1892cde22e97f07bfa4ae98f1
lucasduartedeveloper@gmail.com <lucasduartedeveloper@gmail.com>
2022-01-11 23:18:33 -0300

    first commit

--- index.php
+++ index.php
@@ -75,7 +75,7 @@ catch (PDOException $e) {
 <i id="menu" class="bi bi-arrow-down-circle my-float"></i>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>q
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 
