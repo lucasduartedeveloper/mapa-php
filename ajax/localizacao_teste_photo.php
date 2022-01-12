4bb70cc8b30ca6d1db079ae809975242fc72ffeb
lucasduartedeveloper@gmail.com <lucasduartedeveloper@gmail.com>
2022-01-11 23:03:53 -0300

    first commit

--- ajax/localizacao_teste_photo.php
+++ ajax/localizacao_teste_photo.php
@@ -7,9 +7,9 @@ try {
     $png = htmlspecialchars($_POST["png"]);
     $nome = htmlspecialchars($_POST["nome"]);
 
-    $sql = "INSERT localizacao_teste (nome, png, latitude, longitude,base64) VALUES ('".$nome."','base64',0 ,0,".$png."')";
-    //$stmt = $pdo->prepare($sql);
-    //$stmt->execute();
+    $sql = "INSERT INTO localizacao_teste (nome, png, latitude, longitude,base64) VALUES ('".$nome."','base64',0,0,'".$png."')";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
 
     echo $sql;
   }
