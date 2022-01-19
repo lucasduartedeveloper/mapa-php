336385331bd204b4c0cdaf1bba11d40771c9d0b3
lucasduartedeveloper@gmail.com <lucasduartedeveloper@gmail.com>
2022-01-19 00:36:40 -0300

    first commit

--- geolocation.js
+++ geolocation.js
@@ -115,7 +115,7 @@ $(document).ready(function() {
                reload();
         });
         countDownDate = getTime();
-        countDownDate.setSeconds(d.getSeconds() - 30);
+        countDownDate.setSeconds(d.getSeconds() + 30);
     };
     foo();
     setInterval(foo, 30000);
