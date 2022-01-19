f05257977cc4b2c58dc616d32f03fa0cf5e28b98
lucasduartedeveloper@gmail.com <lucasduartedeveloper@gmail.com>
2022-01-19 03:19:47 -0300

    first commit

--- index.php
+++ index.php
@@ -81,7 +81,7 @@ catch (PDOException $e) {
 <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
 
 <script src="/script.js?v=123"></script>
-<script src="/geolocation.js?v118"></script> 
+<script src="/geolocation.js?v119"></script> 
 
 <script src="//cdn.jsdelivr.net/npm/eruda"></script>
 <script>eruda.init();</script>
