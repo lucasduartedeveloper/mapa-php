e751dd79b488e18d723db4ed1b553bfb38b2e4c0
lucasduartedeveloper@gmail.com <lucasduartedeveloper@gmail.com>
2022-01-20 17:56:18 -0300

    first commit

--- index.php
+++ index.php
@@ -29,10 +29,10 @@ catch (PDOException $e) {
 <link rel="apple-touch-icon" sizes="76x76" href="/webapp/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="/webapp/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/webapp/favicon-16x16.png">
-<link rel="manifest" href="/webapp/site.webmanifest">
-<link rel="mask-icon" href="/webapp/safari-pinned-tab.svg" color="#c0c8cf">
-<meta name="msapplication-TileColor" content="#c0c8cf">
-<meta name="theme-color" content="#c0c8cf">
+<link rel="manifest" href="/webapp/site.webmanifest?v=0">
+<link rel="mask-icon" href="/webapp/safari-pinned-tab.svg" color="#2f2e40">
+<meta name="msapplication-TileColor" content="#2f2e40">
+<meta name="theme-color" content="#2f2e40">
 
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"/>
 
