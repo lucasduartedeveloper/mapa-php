1f8712bbca10cd40f22c00363250483f84753637
lucasduartedeveloper@gmail.com <lucasduartedeveloper@gmail.com>
2022-01-20 14:45:55 -0300

    first commit

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ catch (PDOException $e) {
 
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"/>
 
-<link rel="stylesheet" href="/css/style.css?v=49">
+<link rel="stylesheet" href="/css/style.css?v=50">
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
 
