9cbf754f1fd2c9013b73dccaec07cbf64e62ba31
lucasduartedeveloper@gmail.com <lucasduartedeveloper@gmail.com>
2022-01-20 22:48:16 -0300

    first commit

--- index.php
+++ index.php
@@ -85,7 +85,7 @@ catch (PDOException $e) {
         <i class="bi bi-check-square-fill"></i></p>
     <p id="motion-info" class="info">devicemotion: 
         <i class="bi bi-check-square"></i></p>
-    <p id="motion-info" class="info">ambientlight: 
+    <p id="light-info" class="info">ambientlight: 
         <i class="bi bi-check-square"></i></p>
      </div>
 </div>
