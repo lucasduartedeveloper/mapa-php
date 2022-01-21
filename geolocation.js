e6a91080f27919d1752eb6be69c71c8e52036c0f
lucasduartedeveloper@gmail.com <lucasduartedeveloper@gmail.com>
2022-01-21 05:48:58 -0300

    first commit

--- geolocation.js
+++ geolocation.js
@@ -256,13 +256,15 @@ function animar() {
     }
 }
 
+var last_text = "";
 var running = 0;
 function type(text) {
-     if (running == 0) {
+     if (running == 0 && text != last_text) {
          $(".typing-wrapper").html("");
          var html = "<div class=\"typing-demo\" style=\"width: " + (text.length+3) + "ch; animation: typing 2s steps(" + (text.length+3) +"), blink .5s step-end infinite alternate;\">" + text + "</div>";
          $(".typing-wrapper").html(html);
          running = 1;
+         last_text = text;
          setInterval(function() { running = 0 }, 5000);
      }
 }
