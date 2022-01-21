57e84b33efcf48a71f1120e115420f68a8f76ddc
lucasduartedeveloper@gmail.com <lucasduartedeveloper@gmail.com>
2022-01-21 02:58:18 -0300

    first commit

--- index.php
+++ index.php
@@ -40,7 +40,7 @@ catch (PDOException $e) {
 
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"/>
 
-<link rel="stylesheet" href="/css/style.css?v=60">
+<link rel="stylesheet" href="/css/style.css?v=61">
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
 
@@ -95,7 +95,7 @@ catch (PDOException $e) {
 
 <div id="box4" class="box3">
     <img id="front" class="front" src="/img/front-0.png"/>
-    <div class="wrapper">
+    <div class="typing-wrapper">
     <div class="typing-demo">
        Iluminação forte detectada
     </div>
