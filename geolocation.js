4a25966fbd5823a333279f63f50ebf470c483da5
lucasduartedeveloper@gmail.com <lucasduartedeveloper@gmail.com>
2022-01-22 22:40:52 -0300

    first commit

--- geolocation.js
+++ geolocation.js
@@ -238,11 +238,11 @@ function type(text) {
 var last_text2 = "";
 var running2 = 0;
 function say(text) {
-         if (text != last_text2 && running == 0 &&) {
+         if (text != last_text2 && running2 == 0) {
               // Teste SpeechSynthesisUtterance
-              //running2 = 2;
+              running2 = 2;
               last_text2 = text;
-              //setTimeout(function() { running2 = 0; }, 5000);
+              setTimeout(function() { running2 = 0; }, 5000);
 
               var msg = new SpeechSynthesisUtterance();
               msg.text = text;
