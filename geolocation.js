367802120b0aca35d6f7f3189180720f4eb159ab
lucasduartedeveloper@gmail.com <lucasduartedeveloper@gmail.com>
2022-01-25 19:30:54 -0300

    first commit

--- geolocation.js
+++ geolocation.js
@@ -312,13 +312,11 @@ $("#reset").click(function() {
       $.getJSON("/ajax/localizacao_gps.php?delete=true", function(data) {
           //say("");
           for (var k in itens) {
-               if (itens[k].lat == pos.lat && itens[k].lng == pos.lng) {
-                   $.post("/ajax/localizacao_gps_item.php", {
-                        lat: 0, 
-                        lng: 0,
-                        id: itens[k].id,
-                    });
-               }
+               $.post("/ajax/localizacao_gps_item.php", {
+                    lat: 0, 
+                    lng: 0,
+                    id: itens[k].id,
+               });
           }
       });
 });
