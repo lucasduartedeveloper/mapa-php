0cf2ca328f393adef0c536cb30d202cf0f1e23e9
lucasduartedeveloper@gmail.com <lucasduartedeveloper@gmail.com>
2022-01-24 23:02:50 -0300

    first commit

--- index.php
+++ index.php
@@ -103,8 +103,7 @@ catch (PDOException $e) {
      </div>
 </div>
 
-<div id="box4" class="box3">    
-    <p class="contador">GPS SUPREMO DO VEDITA</p>
+<div id="box4" class="box3">
     <p class="contador">v115.136.264</p>
     <img id="front" class="front" src="/img/icone.png"/>
     <div hidden class="typing-wrapper"></div>
