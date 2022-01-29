22153c134a02e862c1647e0c197e7e42f067a0d7
lucasduartedeveloper@gmail.com <lucasduartedeveloper@gmail.com>
2022-01-29 19:05:50 -0300

    first commit

--- index.php
+++ index.php
@@ -113,7 +113,7 @@ catch (PDOException $e) {
 
 <div id="box4" class="box3">
     <p class="contador">MAP CLEANER</p>
-    <p class="contador">v135.139.397 test</p>
+    <p class="contador">v135.139.398 test</p>
     <img id="front" class="front" src="/img/logo.png"/>
     <div hidden class="typing-wrapper"></div>
      <p  class="velocidade" class="contador">0 m/s</p>
@@ -134,7 +134,7 @@ catch (PDOException $e) {
 <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
 
 <script src="/script.js?v=139"></script>
-<script src="/geolocation.js?v397"></script> 
+<script src="/geolocation.js?v398"></script> 
 
 <script src="//cdn.jsdelivr.net/npm/eruda"></script>
 <script>eruda.init();</script>
