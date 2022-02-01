88b9b2e424a1785f783b5ceb4d433fdbaf30669f
lucasduartedeveloper@gmail.com <lucasduartedeveloper@gmail.com>
2022-02-01 15:24:27 -0300

    first commit

--- geolocation.js
+++ geolocation.js
@@ -838,7 +838,8 @@ function desenharVoldemort() {
      console.log("a:" + a);
      console.log("b:" + b);
      console.log("a2:" + a2);
-     console.log("b2:" + b2);
+     console.log(reguas[0].longitude + b2);
+     
 
      markerLight.setRadius((h/((x+y)/2)) * luz);
      voldemort.marker.setLatLng(new L.LatLng(pos.lat, pos.lng));
