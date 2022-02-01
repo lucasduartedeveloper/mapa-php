6016e643d5937e72c96881b58b37bd62251bfe88
lucasduartedeveloper@gmail.com <lucasduartedeveloper@gmail.com>
2022-01-31 23:44:59 -0300

    first commit

--- index.php
+++ index.php
@@ -114,7 +114,7 @@ catch (PDOException $e) {
 
 <div id="box4" class="box3">
     <p class="contador">MAP BROS</p>
-    <p class="contador">v137.139.452 final*</p>
+    <p class="contador">v137.139.453 final*</p>
     <img id="front" class="front" src="/img/logo-v2.png"/>
     <div hidden class="typing-wrapper"></div>
      <p  class="velocidade" class="contador">0 cm/s</p>
@@ -140,7 +140,7 @@ catch (PDOException $e) {
         crossorigin="anonymous"></script>
 
 <script src="/script.js?v=139"></script>
-<script src="/geolocation.js?v452"></script> 
+<script src="/geolocation.js?v453"></script> 
 
 <script src="//cdn.jsdelivr.net/npm/eruda"></script>
 <script>eruda.init();</script>
