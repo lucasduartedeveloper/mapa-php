2405371e58488a13dc9ad5f056dcd1523ab198ab
lucasduartedeveloper@gmail.com <lucasduartedeveloper@gmail.com>
2022-02-03 14:09:23 -0300

    first commit

--- index.php
+++ index.php
@@ -104,7 +104,7 @@ catch (PDOException $e) {
 
 <div id="box4" class="box3">
     <p class="contador">MAP BROS</p>
-    <p class="contador">v137.141.625 final*</p>
+    <p class="contador">v137.141.626 final*</p>
     <img id="front" class="front" src="/img/logo-v2.png"/>
     <div hidden class="typing-wrapper"></div>
      <p  class="velocidade" class="contador">0 cm/s</p>
@@ -141,7 +141,7 @@ catch (PDOException $e) {
 <script src="https://momentjs.com/downloads/moment.min.js"></script>
 
 <script src="/script.js?v=141"></script>
-<script src="/geolocation.js?v625"></script> 
+<script src="/geolocation.js?v626"></script> 
 
 <script src="//cdn.jsdelivr.net/npm/eruda"></script>
 <script>eruda.init();</script>
