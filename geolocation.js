a3af7b21b4daf2c097121cecdaad620d8a04ab61
lucasduartedeveloper@gmail.com <lucasduartedeveloper@gmail.com>
2022-02-03 01:06:57 -0300

    first commit

--- geolocation.js
+++ geolocation.js
@@ -966,6 +966,11 @@ function validarGrid() {
                    var lat = parseFloat(pos.lat) - (a * k);
                    var lng = parseFloat(pos.lng) - (d *  j);
 
+                   console.log("lat: " + lat);
+                   console.log("lng: " + lng);
+                   console.log("lat: " + grid[m].lat);
+                   console.log("lng: " + grid[m].lng);
+
                    if (grid[m].lat == lat && grid[m].lng == lng) {
                           pontos += 1;
                    }
