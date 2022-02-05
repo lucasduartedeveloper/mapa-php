82c42613eaa6b765b32b69a392503d4d12d13aad
lucasduartedeveloper@gmail.com <lucasduartedeveloper@gmail.com>
2022-02-05 04:58:18 -0300

    first commit

--- index.php
+++ index.php
@@ -40,7 +40,7 @@ catch (PDOException $e) {
 
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"/>
 
-<link rel="stylesheet" href="/css/style.css?v=137">
+<link rel="stylesheet" href="/css/style.css?v=138">
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
 
@@ -104,7 +104,7 @@ catch (PDOException $e) {
 
 <div id="box4" class="box3">
     <p class="contador">マップブラザーズ</p>
-    <p class="contador">v137.143.719 final*</p>
+    <p class="contador">v138.143.719 final*</p>
     <img id="front" class="front" src="/img/logo-v2.png"/>
     <div hidden class="typing-wrapper"></div>
      <p  class="velocidade" class="contador">0 cm/s</p>
