36cf963600efeaff2c7fed93180a8871b86dd86e
* <lucasduartedeveloper@gmail.com>
2022-02-06 01:02:57 -0300

    first commit

--- websocket.js
+++ websocket.js
@@ -1,6 +1,6 @@
-var host = "wss://mapa-ws.herokuapp.com";
+var host = "wss://mapa-ws.herokuapp.com/";
 var ws = new WebSocket(host);
 
 ws.onmessage = (event) => {
-        console.log("Server:" + event.data);
+        console.log("Server: " + event.data);
 };
