99c2f1e26a161374a484f6b1c235cfcab8bb26e4
* <lucasduartedeveloper@gmail.com>
2022-02-07 13:12:50 -0300

    first commit

--- extra/ajax/audio.php
+++ extra/ajax/audio.php
@@ -1,6 +1,51 @@
-<?php include ("/config/db.php")?>
 <?php
 $sql ="";
-$echo = "teste";
+try {
+  include ("/config/db.php");
 
+  if (!empty($_POST["base64"])) {
+
+    $nome = htmlspecialchars($_POST["nome"]);
+    $latitude = htmlspecialchars($_POST["latitude"]);
+    $longitude = htmlspecialchars($_POST["longitude"]);
+    $desenho = htmlspecialchars($_POST["desenho"]);
+    $base64 = htmlspecialchars($_POST["base64"]);
+
+    $sql = "INSERT extra_audio (nome,latitude,longitude,desenho,base64) VALUES ('".$nome."','".$latitude."',''".$longitude."','".$desenho."','".$base64."');";
+
+    $stmt = $pdo->prepare($sql);
+    //$stmt->execute();
+
+    echo $sql;
+  }
+  else if (!empty($_GET["id"])) {
+    $id = htmlspecialchars($_GET["id"]);
+    $sql = "SELECT * FROM extra_audio WHERE id=".$id.";";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    $rowCount = $stmt->rowCount();
+    $details = $stmt->fetchAll(); 
+  
+    echo json_encode($details);
+  }
+  else {
+    $sql = "SELECT id,nome,latitude,longitude,desenho FROM extra_audio;";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    $rowCount = $stmt->rowCount();
+    $details = $stmt->fetchAll(); 
+
+    echo json_encode($details);
+  }
+}
+catch (PDOException $e) {
+   echo 'Connection failed: ' . $e->getMessage();
+   echo $sql;
+}
+catch (Exception $e) {
+    echo 'Error: ' . $e->getMessage();
+    echo $sql;
+}
 ?>
