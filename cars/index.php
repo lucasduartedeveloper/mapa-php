ad3f4c3d487cd9cc74bdbffa54ffcb53843f6756
* <lucasduartedeveloper@gmail.com>
2022-02-07 01:03:07 -0300

    first commit

--- cars/index.php
+++ cars/index.php
@@ -16,7 +16,7 @@
 
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"/>
 
-<link rel="stylesheet" href="/cars/css/style.css?v=8">
+<link rel="stylesheet" href="/cars/css/style.css?v=9">
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
 
