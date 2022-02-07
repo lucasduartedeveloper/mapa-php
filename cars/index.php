5b504702b390521c768465a5248915b34e52fb85
* <lucasduartedeveloper@gmail.com>
2022-02-06 22:24:05 -0300

    first commit

--- cars/index.php
+++ cars/index.php
@@ -49,7 +49,7 @@
 
 <script src="/cars/script/websocket.js?v=0"></script>
 <script src="/cars/script/script.js?v=0"></script>
-<script src="/cars/script/map.js?v=2"></script> 
+<script src="/cars/script/map.js?v=3"></script> 
 
 <script src="//cdn.jsdelivr.net/npm/eruda"></script>
 <script>eruda.init();</script>
--- cars/script/map.js
+++ cars/script/map.js
@@ -9,3 +9,30 @@ var tileLayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoibHVjYXNkdWFydGUxOTkyIiwiYSI6ImNreGZieWE3ODFwNTQyb3N0cW4zNHMxMG8ifQ.HXS54wWrm6wPz-29LVVRbg'
 }).addTo(map);
+
+// Teste REV.AI
+$.ajax({
+    url: "https://api.rev.ai/speechtotext/v1/jobs",
+    method: "POST",
+    dataType: "json",
+    crossDomain: true,
+    contentType: "application/json; charset=utf-8",
+    data: {
+       media_url : "https://www.rev.ai/FTC_Sample_1.mp3",
+       metadata : "This is a sample submit jobs option" 
+    }
+    cache: false,
+    beforeSend: function (xhr) {
+        /* Authorization header */
+        xhr.setRequestHeader("Authorization", "Bearer 02Bt2V6ID8LqsHOO6HyvGH-SCLbge8CHiLYROtMOjRAydDiv9eox0QqLErOKMSLEW24RTBoUjtjDo25qJnR28R8GUbB6s");
+        xhr.setRequestHeader("X-Mobile", "false");
+    },
+    success: function (data) {
+         console.log(data);
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+         console.log(jqXHR);
+         console.log(textStatus);
+         console.log(errorThrown);
+    }
+});
