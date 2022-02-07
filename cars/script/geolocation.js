0fadddedd1945722ad06b0a6fbf760702dcd32f3
* <lucasduartedeveloper@gmail.com>
2022-02-06 21:28:06 -0300

    first commit

--- cars/script/geolocation.js
+++ cars/script/geolocation.js
@@ -1,7 +1,7 @@
 // Create the map
 var map = L.map('map').setView([0, 0], 13);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+var tileLayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 20,
     id: 'mapbox/streets-v11',
