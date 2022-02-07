e53ddd68bca0e71b9babad0583ee33d82fef396d
* <lucasduartedeveloper@gmail.com>
2022-02-06 22:33:33 -0300

    first commit

--- cars/index.php
+++ cars/index.php
@@ -49,7 +49,7 @@
 
 <script src="/cars/script/websocket.js?v=0"></script>
 <script src="/cars/script/script.js?v=0"></script>
-<script src="/cars/script/map.js?v=4"></script> 
+<script src="/cars/script/map.js?v=5"></script> 
 
 <script src="//cdn.jsdelivr.net/npm/eruda"></script>
 <script>eruda.init();</script>
