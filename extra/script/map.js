90958ebe347ab939e8c728c21e04d8f390de420b
* <lucasduartedeveloper@gmail.com>
2022-02-07 03:03:27 -0300

    first commit

--- extra/script/map.js
+++ extra/script/map.js
@@ -14,7 +14,7 @@ var tileLayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}
 var base64anterior = "xxx";
 function postAudio(nome, base64) {
       console.log("nome: " + nome);
-      console.log("base64: " + base64.substring(0,30);
+      console.log("base64: " + base64.substring(0,30));
       console.log("similaridade: " +
            compararStrings(
                  base64anterior, 
