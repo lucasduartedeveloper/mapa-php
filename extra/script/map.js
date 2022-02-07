5b70d7ac4295e3d83fcb52d3008280bb4ba1f029
* <lucasduartedeveloper@gmail.com>
2022-02-07 11:07:32 -0300

    first commit

--- extra/script/map.js
+++ extra/script/map.js
@@ -51,7 +51,7 @@ function formatarAudio(buffer) {
                   }
             }
 
-            novoArray.push(Math.floor((100 / 65535) * (bloco / tamanhoBloco))));
+            novoArray.push(Math.floor((100 / 65535) * (bloco / tamanhoBloco)));
        }
 
        desenharWave(novoArray);
