b6f0fe499f5465fc52cebdb083c9836cd1155682
* <lucasduartedeveloper@gmail.com>
2022-02-08 14:17:11 -0300

    first commit

--- extra/index.php
+++ extra/index.php
@@ -16,7 +16,7 @@
 
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"/>
 
-<link rel="stylesheet" href="/extra/css/style.css?v=36">
+<link rel="stylesheet" href="/extra/css/style.css?v=37">
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
 
@@ -58,10 +58,10 @@
       <i class="bi bi-joystick"></i>
 </button>
 
-<img class="icone-v2" src="/extra/icone-v3.png"/>
+<img class="icone-v3" src="/extra/icone-v3.png"/>
 <p id="version-info" class="version-info">
      FOUND AUDIOS & GPS LTDA<br>
-     v36.0.0.133
+     v37.0.0.133
 </p>
 
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
