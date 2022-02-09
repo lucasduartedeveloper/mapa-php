c61efd334d2adfeafad3cd9e53cd934cdc81dbcc
* <lucasduartedeveloper@gmail.com>
2022-02-09 13:06:41 -0300

    first commit

---
+++ extra/script/login.js
@@ -0,0 +1,12 @@
+// LOGIN
+var myStorage = window.localStorage;
+var cor = myStorage.getItem("cor");
+$(document).ready(function() {
+     if (cor) {
+          cor = prompt("1 - Azul, 2 - Preto, 3 -Amarelo, 4 - Roxo","");
+          myStorage.setItem("cor", cor);
+     }
+
+     
+});
+
