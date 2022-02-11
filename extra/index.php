a58ddf854024684941925c5bc82d2b6038350282
* <lucasduartedeveloper@gmail.com>
2022-02-11 01:09:25 -0300

    first commit

--- extra/index.php
+++ extra/index.php
@@ -21,7 +21,7 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
 <link rel="stylesheet" href="/extra/css/dark-osm.css">
-<link rel="stylesheet" href="/extra/css/style.css?v=54">
+<link rel="stylesheet" href="/extra/css/style.css?v=55">
 
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -69,7 +69,7 @@
 <img id="update" class="icone-v3" src="/extra/icone-v2.png"/>
 <p id="version-info" class="version-info">
      JUPS<br>
-     v54.1.1.266
+     v55.1.1.266
 </p>
 
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
