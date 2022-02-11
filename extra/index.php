776fcc9d9804860ffb9ce0b29c944f9cd52571a9
* <lucasduartedeveloper@gmail.com>
2022-02-11 01:51:58 -0300

    first commit

--- extra/index.php
+++ extra/index.php
@@ -69,7 +69,7 @@
 <img id="update" class="icone-v3" src="/extra/icone-v2.png"/>
 <p id="version-info" class="version-info">
      JUPS<br>
-     v57.1.1.268
+     v57.1.1.269
 </p>
 
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
@@ -91,7 +91,7 @@
 <script src="/extra/script/websocket.js?v=1"></script>
 
 <script src="/extra/script/login.js?v=1"></script> 
-<script src="/extra/script/map.js?v=268"></script> 
+<script src="/extra/script/map.js?v=269"></script> 
 
 <!-- Modal -->
 <div style="z-index: 10001" class="modal fade" id="cameraModal" tabindex="-1" role="dialog" aria-hidden="true">
