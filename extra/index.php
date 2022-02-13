d76c2e1ef1a2df8048f45b7b6b72561c3ad0a17b
* <lucasduartedeveloper@gmail.com>
2022-02-13 02:40:58 -0300

    first commit

--- extra/index.php
+++ extra/index.php
@@ -85,7 +85,7 @@
 
 <p id="version-info" class="version-info">
      LOST RPG<br>
-     v78.1.1.399
+     v78.1.1.400
 </p>
 
 <button id="left" type="button" class="btn-left">
@@ -123,7 +123,7 @@
 <script src="/extra/script/websocket.js?v=1"></script>
 
 <script src="/extra/script/login.js?v=1"></script> 
-<script src="/extra/script/map.js?v=399"></script> 
+<script src="/extra/script/map.js?v=400"></script> 
 
 <!-- Modal -->
 <div style="z-index: 10001" class="modal fade" id="cameraModal" tabindex="-1" role="dialog" aria-hidden="true">
