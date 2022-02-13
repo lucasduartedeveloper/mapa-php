2141413961dde50fe9d2d7437aa97e31b2abe304
* <lucasduartedeveloper@gmail.com>
2022-02-13 03:28:21 -0300

    first commit

--- extra/index.php
+++ extra/index.php
@@ -84,8 +84,8 @@
 </p>
 
 <p id="version-info" class="version-info">
-     PARADOS E FURIOSOS<br>
-     v78.1.1.406
+     P & F<br>
+     v78.1.1.407
 </p>
 
 <button id="left" type="button" class="btn-left">
@@ -123,7 +123,7 @@
 <script src="/extra/script/websocket.js?v=1"></script>
 
 <script src="/extra/script/login.js?v=1"></script> 
-<script src="/extra/script/map.js?v=406"></script> 
+<script src="/extra/script/map.js?v=407"></script> 
 
 <!-- Modal -->
 <div style="z-index: 10001" class="modal fade" id="cameraModal" tabindex="-1" role="dialog" aria-hidden="true">
