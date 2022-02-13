a1c50253287afbb9b2b7f2096e6f3f6bbd0c2897
* <lucasduartedeveloper@gmail.com>
2022-02-13 02:10:52 -0300

    first commit

--- extra/index.php
+++ extra/index.php
@@ -123,7 +123,7 @@
 <script src="/extra/script/websocket.js?v=1"></script>
 
 <script src="/extra/script/login.js?v=1"></script> 
-<script src="/extra/script/map.js?v=395"></script> 
+<script src="/extra/script/map.js?v=396"></script> 
 
 <!-- Modal -->
 <div style="z-index: 10001" class="modal fade" id="cameraModal" tabindex="-1" role="dialog" aria-hidden="true">
