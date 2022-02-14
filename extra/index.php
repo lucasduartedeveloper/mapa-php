dde02b36ebbf12b6fcba461c8f66de326679bdbb
* <lucasduartedeveloper@gmail.com>
2022-02-14 05:28:46 -0300

    first commit

--- extra/index.php
+++ extra/index.php
@@ -20,7 +20,7 @@
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
-<link rel="stylesheet" href="/extra/css/style.css?v=80">
+<link rel="stylesheet" href="/extra/css/style.css?v=81">
 
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -88,8 +88,7 @@
 </p>
 
 <p id="version-info" class="version-info">
-     P & F<br>
-     v80.3.1.484
+     v81.3.1.484
 </p>
 
 <button id="left" type="button" class="btn-left">
@@ -127,7 +126,7 @@
 <script src="/extra/script/websocket.js?v=3"></script>
 
 <script src="/extra/script/login.js?v=1"></script> 
-<script src="/extra/script/map.js?v=483"></script> 
+<script src="/extra/script/map.js?v=484"></script> 
 
 <!-- Modal -->
 <div style="z-index: 10001" class="modal fade" id="cameraModal" tabindex="-1" role="dialog" aria-hidden="true">
