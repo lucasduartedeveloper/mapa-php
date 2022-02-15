7e7d75b763cdbabaf5e81b6a6c7aaa3cbee146bf
* <lucasduartedeveloper@gmail.com>
2022-02-15 02:53:23 -0300

    first commit

--- extra/script/map.js
+++ extra/script/map.js
@@ -517,15 +517,6 @@ function carregarAudio(m) {
 
 // Excluir áudio
 function excluirAudio(m) {
-
-    // Comparar o Nv
-    if(audios[m].desenho.split(",").length > trajetos[playerId].length) {
-        audio.pause();
-        audio = new Audio("/extra/audio/game_over.mp3");
-        audio.play();
-        return;
-    }
-
     var pointList = [];
     map.removeControl(wire);
 
