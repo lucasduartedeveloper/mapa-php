82e6e40207cc4d5a6b5a2963790ce04b13889e43
* <lucasduartedeveloper@gmail.com>
2022-02-14 22:26:40 -0300

    first commit

--- extra/index.php
+++ extra/index.php
@@ -88,7 +88,7 @@
 </p>
 
 <p id="version-info" class="version-info">
-     v82.3.1.507
+     v82.3.1.508
 </p>
 
 <button id="left" type="button" class="btn-left">
@@ -126,7 +126,7 @@
 <script src="/extra/script/websocket.js?v=3"></script>
 
 <script src="/extra/script/login.js?v=1"></script> 
-<script src="/extra/script/map.js?v=507"></script> 
+<script src="/extra/script/map.js?v=508"></script> 
 
 <!-- Modal -->
 <div style="z-index: 10001" class="modal fade" id="cameraModal" tabindex="-1" role="dialog" aria-hidden="true">
