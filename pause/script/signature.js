781b0b1aa12c390c8a267ac9a85828d0067a4d22
* <lucasduartedeveloper@gmail.com>
2022-02-16 16:35:01 -0300

    first commit

--- pause/script/signature.js
+++ pause/script/signature.js
@@ -1,24 +1,41 @@
 var playerId = new Date().getTime();
+var audio0 = new Audio("audio/game_notication.wav");
 
 $(document).ready(function() {
-    $("#player-id").text(playerId);
-
     $("#signature").jqScribble();
     $("#signature").data('jqScribble').update({
          width: 300, height: 200
     });
     $("#signature").on("touchend", function(e) {
          var dataUrl = signature.toDataURL();
-         ws.send("PAUSE|"+playerId+"|"+dataUrl);
+         ws.send("PAUSE|"+playerId+"|CANVAS|"+dataUrl);
     });
     ws.onmessage = function(e) {
         var msg = e.data.split("|");
-        if (msg[0] == "PAUSE" && playerId != msg[1]) {
+        if (msg[0] == "PAUSE" &&
+            playerId != msg[1]) {
 
-             $("#signature").data('jqScribble').update({
-                   backgroundImage: msg[2],
-                   width: 300, height: 200
-             });
+            if (msg[2] == "CANVAS") {
+                  $("#signature").data('jqScribble').update({
+                        backgroundImage: msg[2],
+                        width: 300, height: 200
+                 });
+            }
+            else if (msg[2] == "LIKE") {
+                 audio0.play();
+            }
+            else if (msg[2] == "DISLIKE") {
+                 $("#signature").data('jqScribble').clear();
+                 audio0.play();
+            }
         }
     };
+
+    $("#btn-like").click(function() {
+        ws.send("PAUSE|"+playerId+"|LIKE");
+    });
+    $("#btn-dislike").click(function() {
+        ws.send("PAUSE|"+playerId+"|DISLIKE");
+    });   
+    $("#player-id").text(playerId);
 });
