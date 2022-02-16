006a70fff4b927ddcc906cc8227eda938ae36d51
* <lucasduartedeveloper@gmail.com>
2022-02-16 05:19:17 -0300

    first commit

--- pause/script/signature.js
+++ pause/script/signature.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
     ws.onmessage = function(e) {
         var msg = e.data.split("|");
         if (msg[0] == "PAUSE") {
-             console.log(msg[1]);
+             //console.log(msg[1]);
              $("#signature").data('jqScribble').update({
                    backgroundImage: msg[1]
              });
