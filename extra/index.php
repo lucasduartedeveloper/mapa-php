bfbafb249eb1d00f61178653acb1b91a3cba87d2
* <lucasduartedeveloper@gmail.com>
2022-02-15 23:34:17 -0300

    first commit

--- extra/index.php
+++ extra/index.php
@@ -20,7 +20,7 @@
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
-<link rel="stylesheet" href="/extra/css/style.css?v=95">
+<link rel="stylesheet" href="/extra/css/style.css?v=96">
 
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -100,7 +100,7 @@
 </p>
 
 <p id="version-info" class="version-info">
-     v95.8.1.561
+     v96.8.1.561
 </p>
 
 <button id="left" type="button" class="btn-left">
