665676925d3e2cbf8144acf35de1a38d818ee4ac
* <lucasduartedeveloper@gmail.com>
2022-02-16 17:36:02 -0300

    first commit

--- pause/index.php
+++ pause/index.php
@@ -20,7 +20,7 @@
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
-<link rel="stylesheet" href="css/style.css?v=8">
+<link rel="stylesheet" href="css/style.css?v=9">
 
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -48,7 +48,7 @@
 </div>
 
 <p id="version-info">
-     v8.2.26
+     v9.2.26
      <br>
      <span id="server-info">
      CONNECTING...
