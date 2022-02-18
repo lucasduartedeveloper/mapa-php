1dc867e0e50809d87b8baf02c9308308d4165651
* <lucasduartedeveloper@gmail.com>
2022-02-18 16:38:12 -0300

    first commit

--- camera/index.php
+++ camera/index.php
@@ -20,7 +20,7 @@
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
-<link rel="stylesheet" href="css/style.css?v=32">
+<link rel="stylesheet" href="css/style.css?v=0">
 
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -34,7 +34,6 @@
      <img id="logo" src="webapp/android-chrome-192x192.png"/>
      <img id="hand" src="img/hand.png"/>
      <span id="title">???</span>
-     <img id="fire" src="img/fire-ball.gif"/>
 </p>
 
 <video style="display:none" id="video" width="300" height="200" autoplay></video>
