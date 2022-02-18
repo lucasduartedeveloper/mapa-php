7eba26713af270016b18c62abaec43f34b0f5a75
* <lucasduartedeveloper@gmail.com>
2022-02-18 18:14:03 -0300

    first commit

--- kindle/index.php
+++ kindle/index.php
@@ -6,10 +6,18 @@
 
      </style>
      <script>
-
+          var updated = 0;
+          setInterval(function() {
+                updated += 1;
+                document
+                .getElementById("update-test")
+                .innerHtml = updated;
+          }, 250);
      </script>
      </head>
      <body>
-
+         <p id="update-test">
+             0
+         </p>
      </body>
 </html>
