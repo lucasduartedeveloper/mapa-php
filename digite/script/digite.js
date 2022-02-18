5d444085d3b7a9a9d2c70227c2ab02298639bf02
* <lucasduartedeveloper@gmail.com>
2022-02-18 01:27:40 -0300

    first commit

--- digite/script/digite.js
+++ digite/script/digite.js
@@ -1,32 +1,42 @@
 var audio0 = new Audio("audio/game_notification.wav");
 var audio1 = new Audio("audio/sfx_victory.wav");
 var audio2 = new Audio("audio/game_over.wav");
+var audio3 = new Audio("audio/getting_hit.wav");
+var audio4 = new Audio("audio/creature_dying.wav");
 
 var words = [
-   "CACHORRO",
-   "GATO",
-   "JACARÉ",
-   "FLAMINGO",
-   "DINOSSAURO",
-   "MISERICÓRDIA",
-   "LANCHE",
-   "CAFÉ",
-   "CIGARRO",
-   "CARRO",
-   "MOTO",
-   "BICICLETA"
+   "BOLA DE FOGO",
+   "BOLA DE ÁGUA",
+   "BOLA DE DINOSSAURO",
+   "VOADORA DE GELO",
+   "SOCO DA FLORESTA",
+   "AMOR FORÇADO"
 ];
 
 var word = getRandomWord();
+var playerId = new Date().getTime();
+var points = 0;
 
 $(document).ready(function() {
+    ws.onmessage = function(e) {
+        var msg = e.data.split("|");
+        if (msg[0] == "DIGITE" &&
+            playerId != msg[1]) {
+            if (msg[2] == "ADD_POINT") {
+                 points += 1;
+                 audio3.play();
+            }
+        }
+    };
+
     $("#btn-done").on("click", function() {
          if ($("input").val().toUpperCase() == word) {
-               audio0.play();
+               audio3.play();
                word = getRandomWord();
                drawBoard();
                $("input").val("");
                $("input").focus();
+               ws.send("DIGITE|"+playerId+"|ADD_POINT");
          }
          else {
               audio2.play();
