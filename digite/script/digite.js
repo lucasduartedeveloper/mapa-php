5d5d9ba7bbec603d1cfaffa567b6c941d72bcdaa
* <lucasduartedeveloper@gmail.com>
2022-02-17 23:12:41 -0300

    first commit

--- digite/script/digite.js
+++ digite/script/digite.js
@@ -10,7 +10,7 @@ var words = [
 var word = getRandomWord();
 
 $(document).ready(function() {
-    $("input").change(function() {
+    $("input").on("change", function() {
          if ($("input").val() == word) {
                getRandomWord();
                drawBoard();
