e937c7bb364a1d602a68616ae3538b88c5c1d28c
* <lucasduartedeveloper@gmail.com>
2022-02-18 05:10:25 -0300

    first commit

--- digite/index.php
+++ digite/index.php
@@ -20,7 +20,7 @@
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
-<link rel="stylesheet" href="css/style.css?v=24">
+<link rel="stylesheet" href="css/style.css?v=25">
 
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -55,7 +55,7 @@
 </div>
 
 <p id="version-info">
-     v24.0.34
+     v25.0.34
      <br>
      <span id="server-info">
      CONNECTING...
