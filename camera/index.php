55bbfbac1651ab9ebfd4f66df6d8418ea3e87a83
* <lucasduartedeveloper@gmail.com>
2022-02-18 23:35:34 -0300

    first commit

--- camera/index.php
+++ camera/index.php
@@ -40,7 +40,7 @@
 <canvas width="300" height="200" id="camera-canvas"></canvas>
 
 <p id="version-info">
-     v3.1.7
+     v3.1.8
      <br>
      <span id="server-info">
      CONNECTING...
@@ -71,7 +71,7 @@
 
 <script src="script/websocket.js?v=0"></script>
 <script src="script/database.js?v=1"></script>
-<script src="script/camera.js?v=7"></script>
+<script src="script/camera.js?v=8"></script>
 
 <script src="//cdn.jsdelivr.net/npm/eruda"></script>
 <script>eruda.init();</script>
