15e69542fc0512c93e207583c8eb5f4475984b24
* <lucasduartedeveloper@gmail.com>
2022-02-20 00:25:43 -0300

    first commit

--- game/script/game.js
+++ game/script/game.js
@@ -3,10 +3,12 @@ var gears {
     { id: "gear-1",posX: 0, posY: 0 },
     { id: "gear-2",posX: 0, posY: 0 },
 }
-var gear = gears[0];
+var selected = gears[0];
 
 $(document).ready(function() {
-    $(".gear").on{"touchstart", function(e) {
-         console.log(e);
+    $(".gear").on("touchstart", function(e) {
+         console.log(e.target.id);
+         selected = gears.filter((e) => e.id == e.target.id);
+         console.log(selected);
     });
 });
