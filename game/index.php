7a9ab27c10032462b25a1c66ecc9a68932298ad6
* <lucasduartedeveloper@gmail.com>
2022-02-20 00:00:59 -0300

    first commit

--- game/index.php
+++ game/index.php
@@ -20,7 +20,7 @@
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
-<link rel="stylesheet" href="css/style.css?v=8">
+<link rel="stylesheet" href="css/style.css?v=9">
 
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -36,7 +36,7 @@
 <img class="gear spin" src="img/gear.png" />
 
 <p id="version-info">
-     v8.0.0
+     v9.0.0
      <br>
      <span id="server-info">
      CONNECTING...
