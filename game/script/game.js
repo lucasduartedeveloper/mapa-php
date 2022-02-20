2b2cde155cf80ac28a8397667c17536cb61d94c9
* <lucasduartedeveloper@gmail.com>
2022-02-20 00:41:52 -0300

    first commit

--- game/script/game.js
+++ game/script/game.js
@@ -1,14 +1,31 @@
-var gears {
-    { id: "gear-0", posX: 0, posY: 0 },
-    { id: "gear-1",posX: 0, posY: 0 },
-    { id: "gear-2",posX: 0, posY: 0 },
+var gears = {
+    { id: "gear-0", pageX: 0, pageY: 0 },
+    { id: "gear-1",pageX: 0, pageY: 0 },
+    { id: "gear-2",pageX: 0, pageY: 0 },
 }
 var selected = gears[0];
+var playing = false;
 
 $(document).ready(function() {
     $(".gear").on("touchstart", function(e) {
+         playing = true;
          console.log(e.target.id);
          selected = gears.filter((e) => e.id == e.target.id);
          console.log(selected);
+
+         selected.pageX = 
+               e.originalEvent.touches[0].pageX;
+         selected.pageY = 
+               e.originalEvent.touches[0].pageY;
+         console.log(selected);
+    });
+    
+    $(".gear").on("touchmove", function(e) {
+         selected.pageX = 
+               e.originalEvent.touches[0].pageX;
+         selected.pageY = 
+               e.originalEvent.touches[0].pageY;
+         console.log(selected);
     });
 });
+
