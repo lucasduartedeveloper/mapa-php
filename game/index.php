40f1dcc1580d9a688c984390704cf37be6574a71
* <lucasduartedeveloper@gmail.com>
2022-02-19 23:29:31 -0300

    first commit

--- game/index.php
+++ game/index.php
@@ -20,7 +20,7 @@
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
-<link rel="stylesheet" href="css/style.css?v=0">
+<link rel="stylesheet" href="css/style.css?v=1">
 
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
