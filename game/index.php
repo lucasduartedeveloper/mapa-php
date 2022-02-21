b4db5fc368bc33a174ed0724a349e6dbfc88fc2f
* <lucasduartedeveloper@gmail.com>
2022-02-20 22:00:08 -0300

    first commit

--- game/index.php
+++ game/index.php
@@ -21,7 +21,7 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
 <!-- <link rel="stylesheet" href="css/normalizee.css"> -->
-<link rel="stylesheet" href="css/style.css?v=52">
+<link rel="stylesheet" href="css/style.css?v=53">
 
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -36,7 +36,7 @@
 </p>
 
 <div class="heart-border">
-    <img class="heart beat"/>
+    <img class="heart beat" src="img/heart.png"/>
 </div>
 <div class="gears">
     <div class="gear-place-container">
@@ -60,7 +60,7 @@
 </div>
 
 <p id="version-info">
-     v52.5.41.1
+     v53.5.41.1
      <br>
      <span id="server-info">
      CONNECTING...
