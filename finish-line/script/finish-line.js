0fb91ba847387b905062ec8e3b9dcf9d79fefcb9
* <lucasduartedeveloper@gmail.com>
2022-02-23 02:18:11 -0300

    first commit

---
+++ finish-line/script/finish-line.js
@@ -0,0 +1,12 @@
+// Botão de gravação
+$(document).ready(function() {
+    $(".finish-line").click(function(e) {
+         var html = 
+         "<div class=\"black\"></div>" +
+         "<div class=\"white\"></div>";
+         for (var k = 0; k < 500; k++) {
+              $(".finish-line").html(
+              $(".finish-line").html() + html);
+         }
+    });
+});
