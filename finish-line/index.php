88fabd4d12d8e8159b04088918e9798616179212
* <lucasduartedeveloper@gmail.com>
2022-02-23 05:48:54 -0300

    first commit

--- finish-line/index.php
+++ finish-line/index.php
@@ -40,7 +40,7 @@
 <div id="red"></div>
 
 <p style="display:none" id="version-info">
-     v6.0.18
+     v6.0.19
      <br>
      <span id="server-info">
      CONNECTING...
@@ -66,7 +66,7 @@
 <script src="https://cdn.rawgit.com/mattdiamond/Recorderjs/08e7abd9/dist/recorder.js"></script>
 
 <script src="script/websocket.js?v=0"></script>
-<script src="script/finish-line.js?v=18"></script>
+<script src="script/finish-line.js?v=19"></script>
 
 <script src="//cdn.jsdelivr.net/npm/eruda"></script>
 <script>eruda.init();</script>
