5e367769885ed2212fd487c65a10087b3b61db17
* <lucasduartedeveloper@gmail.com>
2022-02-23 05:46:41 -0300

    first commit

--- finish-line/index.php
+++ finish-line/index.php
@@ -21,7 +21,7 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
 <!-- <link rel="stylesheet" href="css/normalizee.css"> -->
-<link rel="stylesheet" href="css/style.css?v=5">
+<link rel="stylesheet" href="css/style.css?v=6">
 
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -40,7 +40,7 @@
 <div id="red"></div>
 
 <p style="display:none" id="version-info">
-     v5.0.18
+     v6.0.18
      <br>
      <span id="server-info">
      CONNECTING...
