9f37f5d86ce71817ef395619f463dfb3ede342e9
* <lucasduartedeveloper@gmail.com>
2022-02-23 01:15:14 -0300

    first commit

--- talking-bot/index.php
+++ talking-bot/index.php
@@ -45,7 +45,7 @@
 <input style="display:none" type="file" id="file-selector">
 
 <p id="version-info">
-     v10.0.8
+     v10.0.9
      <br>
      <span id="server-info">
      CONNECTING...
@@ -71,7 +71,7 @@
 <script src="https://cdn.rawgit.com/mattdiamond/Recorderjs/08e7abd9/dist/recorder.js"></script>
 
 <script src="script/websocket.js?v=0"></script>
-<script src="script/talking-bot.js?v=8"></script>
+<script src="script/talking-bot.js?v=9"></script>
 
 <script src="//cdn.jsdelivr.net/npm/eruda"></script>
 <script>eruda.init();</script>
