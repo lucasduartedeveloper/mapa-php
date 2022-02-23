483341faa26169af9eb96e04450339b7a14eb4cc
* <lucasduartedeveloper@gmail.com>
2022-02-23 19:25:53 -0300

    first commit

--- finish-line/script/finish-line.js
+++ finish-line/script/finish-line.js
@@ -75,6 +75,8 @@ $(document).ready(function() {
     $("#blue,#red").on("touchmove", function(e) {
          if (!playing) return false;
 
+         console.log($(e.target));
+
          var blue = $(e.target)[0].id == "blue" ? 0 : 1;
          var red = $(e.target[0]).id == "red" ? 0 : 1;
          
