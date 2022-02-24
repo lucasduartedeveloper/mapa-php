6a1cb52556a27e63612cba3d9583dcfd59c35341
* <lucasduartedeveloper@gmail.com>
2022-02-24 04:44:39 -0300

    first commit

--- talking-bot/index.php
+++ talking-bot/index.php
@@ -21,7 +21,7 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
 <!-- <link rel="stylesheet" href="css/normalizee.css"> -->
-<link rel="stylesheet" href="css/style.css?v=14">
+<link rel="stylesheet" href="css/style.css?v=15">
 
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -48,7 +48,7 @@
 <input style="display:none" type="file" id="file-selector">
 
 <p id="version-info">
-     v14.0.17
+     v15.0.17
      <br>
      <span id="server-info">
      CONNECTING...
