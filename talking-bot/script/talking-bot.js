c5f6c05ce763533bc782169bd5baa6a3de20d5d6
* <lucasduartedeveloper@gmail.com>
2022-02-25 03:50:44 -0300

    first commit

--- talking-bot/script/talking-bot.js
+++ talking-bot/script/talking-bot.js
@@ -226,9 +226,9 @@ function alarme(audio) {
         }
     }
     play = 
-        (Math.abs(accX) > 1 && Math.abs(accX)) < 10 ||
-        (Math.abs(accY) > 1 && Math.abs(accY)) < 10 || 
-        (Math.abs(accZ) > 1 && Math.abs(accZ)) < 10 || 
+        (Math.abs(accX) > 1 && Math.abs(accX) < 10) ||
+        (Math.abs(accY) > 1 && Math.abs(accY) < 10) || 
+        (Math.abs(accZ) > 1 && Math.abs(accZ) < 10) || 
         play;
     if (play) {
         $("#mic").click();
