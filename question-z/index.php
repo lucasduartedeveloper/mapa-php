baa3afb4ebc6e56139627e4333d98a546e12685f
* <lucasduartedeveloper@gmail.com>
2022-02-27 23:17:13 -0300

    first commit

--- question-z/index.php
+++ question-z/index.php
@@ -21,7 +21,7 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
 <!-- <link rel="stylesheet" href="css/normalizee.css"> -->
-<link rel="stylesheet" href="css/style.css?v=2">
+<link rel="stylesheet" href="css/style.css?v=3">
 
 <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
 
@@ -44,8 +44,11 @@
 <p id="option_c" class="option">
     OPÇÃO C: LAURA MITSUKO TANAKA
 </p>
+<p id="option_c" class="option">
+    OPÇÃO D: BÁRBARA HASEGAWA
+</p>
 <p id="option_d" class="option">
-    OPÇÃO D: NENHUMA DAS ALTERNATIVAS
+    OPÇÃO E: NENHUMA DAS ALTERNATIVAS
 </p>
 </div>
 
@@ -60,7 +63,7 @@
 <input style="display:none" type="file" id="file-selector">
 
 <p id="version-info">
-     v2.0.1
+     v3.0.1
      <br>
      <span id="server-info">
      CONNECTING...
