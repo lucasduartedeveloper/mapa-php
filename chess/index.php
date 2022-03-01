485ca2832986f5b78fe41d3218045b72a15f5c79
* <lucasduartedeveloper@gmail.com>
2022-03-01 12:29:26 -0300

    first commit

--- chess/index.php
+++ chess/index.php
@@ -21,7 +21,7 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
 <!-- <link rel="stylesheet" href="css/normalizee.css"> -->
-<link rel="stylesheet" href="css/style.css?v=1">
+<link rel="stylesheet" href="css/style.css?v=2">
 
 <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
 
@@ -50,7 +50,7 @@
 <input style="display:none" type="file" id="file-selector">
 
 <p id="version-info">
-     v1.0.4
+     v2.0.5
      <br>
      <span id="server-info">
      CONNECTING...
@@ -76,7 +76,7 @@
 <script src="https://cdn.rawgit.com/mattdiamond/Recorderjs/08e7abd9/dist/recorder.js"></script>
 
 <script src="script/websocket.js?v=0"></script>
-<script src="script/chess.js?v=4"></script>
+<script src="script/chess.js?v=5"></script>
 
 <script src="//cdn.jsdelivr.net/npm/eruda"></script>
 <script>eruda.init();</script>
