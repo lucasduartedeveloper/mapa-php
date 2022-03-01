d2dfcc6456c7255614d977edf888947927069c2d
* <lucasduartedeveloper@gmail.com>
2022-03-01 14:24:14 -0300

    first commit

--- jumper-frog/index.php
+++ jumper-frog/index.php
@@ -21,7 +21,7 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
 <!-- <link rel="stylesheet" href="css/normalizee.css"> -->
-<link rel="stylesheet" href="css/style.css?v=7">
+<link rel="stylesheet" href="css/style.css?v=8">
 
 <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
 
@@ -53,7 +53,7 @@ JUMPER FROG<br>
 <input style="display:none" type="file" id="file-selector">
 
 <p id="version-info">
-     v7.0.3
+     v8.0.3
      <br>
      <span id="server-info">
      CONNECTING...
