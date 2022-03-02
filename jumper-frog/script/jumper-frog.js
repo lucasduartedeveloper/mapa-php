2d893fc4d216bb51238978bf1e0561dc844b2416
* <lucasduartedeveloper@gmail.com>
2022-03-02 04:16:45 -0300

    first commit

--- jumper-frog/script/jumper-frog.js
+++ jumper-frog/script/jumper-frog.js
@@ -1,7 +1,7 @@
 var audio = new Audio("audio/phone-lock.wav");
 var alarm = new Audio("audio/battleship-alarm.wav");
 var coin = new Audio("audio/coin.wav");
-var frogJump = new Audio("audio/frog-jump.wav");
+var frogJump = new Audio("audio/frog-sfx.wav");
 var victory = new Audio("audio/sfx-victory.wav");
 
 function formatarAudio(buffer) {
