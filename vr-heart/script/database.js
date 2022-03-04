97c42f7bd28486be66bc426bc26d323896553133
* <lucasduartedeveloper@gmail.com>
2022-03-04 10:27:18 -0300

    first commit

--- vr-heart/script/database.js
+++
@@ -1,8 +0,0 @@
-function saveFrame(cameraId, base64) {
-     $.post("ajax/camera.php", {
-             cameraId: cameraId,
-             base64: base64,
-             }).done(function(data) { 
-                     //console.log(data);
-      });
-}
