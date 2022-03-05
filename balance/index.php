bab7269a524869545cf2cad20b5a9938b4e9d5d8
* <lucasduartedeveloper@gmail.com>
2022-03-05 17:56:33 -0300

    first commit

--- balance/index.php
+++ balance/index.php
@@ -21,7 +21,7 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
 <!-- <link rel="stylesheet" href="css/normalizee.css"> -->
-<link rel="stylesheet" href="css/style.css?v=3">
+<link rel="stylesheet" href="css/style.css?v=4">
 
 <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
 
@@ -31,13 +31,13 @@
 </head>
 <body>
 
-<p class="title-2">
+<p id="title">
   CAIXA 84
 </p>
-<p id="title">
+<p id="cpf">
    078.402.969-55
 </p>
-<p id="balance" class="title-2">
+<p id="balance">
    R$ ----
 </p>
 <div>
@@ -53,7 +53,7 @@ Depósito
 </div>
 
 <p id="version-info">
-     v3.0.48
+     v4.0.48
      <br>
      <span id="server-info">
      CONNECTING...
