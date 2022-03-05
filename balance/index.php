62f69eb271db123e3099af6d94778574664361f6
* <lucasduartedeveloper@gmail.com>
2022-03-05 17:52:58 -0300

    first commit

--- balance/index.php
+++ balance/index.php
@@ -21,7 +21,7 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
 <!-- <link rel="stylesheet" href="css/normalizee.css"> -->
-<link rel="stylesheet" href="css/style.css?v=2">
+<link rel="stylesheet" href="css/style.css?v=3">
 
 <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
 
@@ -53,7 +53,7 @@ Depósito
 </div>
 
 <p id="version-info">
-     v2.0.48
+     v3.0.48
      <br>
      <span id="server-info">
      CONNECTING...
