4a4345e0b690debae4e85ff994dd0b9fd37be869
* <lucasduartedeveloper@gmail.com>
2022-03-14 14:04:14 -0300

    first commit

--- maze/index.php
+++ maze/index.php
@@ -38,7 +38,7 @@
 </canvas>
 
 <p id="version-info">
-     MAZE v0.0.8
+     MAZE v0.0.9
      <br>
      <span id="server-info">
      CONNECTING...
@@ -64,7 +64,7 @@
 <script src="https://cdn.rawgit.com/mattdiamond/Recorderjs/08e7abd9/dist/recorder.js"></script>
 
 <script src="script/websocket.js?v=0"></script>
-<script src="script/maze.js?v=8"></script>
+<script src="script/maze.js?v=9"></script>
 
 <script src="//cdn.jsdelivr.net/npm/eruda"></script>
 <script>eruda.init();</script>
