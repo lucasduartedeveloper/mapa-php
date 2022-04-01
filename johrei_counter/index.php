1927bd4f05257a95f8362c1983210c55dd771569
* <lucasduartedeveloper@gmail.com>
2022-03-31 21:53:34 -0300

    first commit

--- johrei_counter/index.php
+++ johrei_counter/index.php
@@ -39,7 +39,7 @@
 </div>
 
 <p id="version-info">
-     JOHREI COUNTER v0.0.1
+     JOHREI COUNTER v0.0.2
      <br>
      <span id="server-info">
      CONNECTING...
@@ -65,7 +65,7 @@
 <script src="https://cdn.rawgit.com/mattdiamond/Recorderjs/08e7abd9/dist/recorder.js"></script>
 
 <script src="script/websocket.js?v=0"></script>
-<script src="script/johrei_counter.js?v=1"></script>
+<script src="script/johrei_counter.js?v=2"></script>
 
 <script src="//cdn.jsdelivr.net/npm/eruda"></script>
 <script>eruda.init();</script>
