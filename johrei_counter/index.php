4c610044de52af9b0d26230bb50d35b914e58176
* <lucasduartedeveloper@gmail.com>
2022-04-02 19:08:19 -0300

    first commit

--- johrei_counter/index.php
+++ johrei_counter/index.php
@@ -41,7 +41,7 @@
 </div>
 <div class="dropdown">
   <button class="btn btn-secondary dropdown-toggle" type="button" id="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    CARREGANDO...
+    NOMES
   </button>
   <div id="" class="dropdown-menu" aria-labelledby="dropdownMenuButton">
   <a class="dropdown-item" href="#">Evelyn</a>
