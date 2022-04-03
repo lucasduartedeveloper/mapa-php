fd5777b9ec09d8e4b375a72d776e9e5f469909e9
* <lucasduartedeveloper@gmail.com>
2022-04-03 14:13:34 -0300

    first commit

---
+++ leaf_counter/ajax/leaf.php
@@ -0,0 +1,36 @@
+<?php include ('config/db.php')?>
+<?php
+$sql ="";
+try {
+  if (isset($_POST["data"])) {
+
+    $quantidade = htmlspecialchars($_POST["quantidade"]);
+    $data = htmlspecialchars($_POST["data"]);
+
+    $sql = "INSERT INTO leaf_counter (data_hora, foto) quantidade=".$quantidade." WHERE data='".$data."'";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+
+    echo $sql;
+  }
+  else {
+    $sql = "SELECT * FROM leaf_counter ORDER BY id DESC";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    $rowCount = $stmt->rowCount();
+    $details = $stmt->fetchAll(); 
+
+    echo json_encode($details);
+  }
+}
+catch (PDOException $e) {
+   echo 'Connection failed: ' . $e->getMessage();
+   echo $sql;
+}
+catch (Exception $e) {
+    echo 'Error: ' . $e->getMessage();
+    echo $sql;
+}
+?>
