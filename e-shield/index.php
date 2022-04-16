7d9d247cc47ac0171587cf3918d409ded7ba43c5
* <lucasduartedeveloper@gmail.com>
2022-04-16 14:12:25 -0300

    first commit

--- e-shield/index.php
+++ e-shield/index.php
@@ -21,7 +21,7 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
 <!-- <link rel="stylesheet" href="css/normalizee.css"> -->
-<link rel="stylesheet" href="css/style.css?v=0">
+<link rel="stylesheet" href="css/style.css?v=1">
 
 <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
 
@@ -45,7 +45,7 @@
 <input style="display:none" type="file" id="file-selector">
 
 <p id="version-info">
-     v0.0.0
+     v1.0.0
      <br>
      <span id="server-info">
      CONNECTING...
