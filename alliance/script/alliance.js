eaac893a5e433ff4cf1a001c5f2ac13d1efbfe2f
* <lucasduartedeveloper@gmail.com>
2022-04-22 09:41:11 -0300

    first commit

---
+++ alliance/script/alliance.js
@@ -0,0 +1,11 @@
+var audio = new Audio("audio/phone-lock.wav");
+var alarm = new Audio("audio/battleship-alarm.wav");
+var coin = new Audio("audio/coin.wav");
+var frogJump = new Audio("audio/frog-sfx.wav");
+var victory = new Audio("audio/sfx-victory.wav");
+
+$(document).ready(function() {
+    
+     
+
+});
