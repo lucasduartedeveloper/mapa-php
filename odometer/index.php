839b2393e75c0c7e3e962c50fdc71fec45512880
* <lucasduartedeveloper@gmail.com>
2022-06-30 16:25:36 -0300

    first commit

--- odometer/index.php
+++ odometer/index.php
@@ -21,7 +21,7 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
 <!-- <link rel="stylesheet" href="css/normalizee.css"> -->
-<link rel="stylesheet" href="css/style.css?v=9">
+<link rel="stylesheet" href="css/style.css?v=10">
 
 <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
 
@@ -51,7 +51,7 @@
 </div>
 
 <p id="version-info">
-     ODOMETER v9.0.7
+     ODOMETER v10.0.7
      <br>
      <span id="server-info">
      CONNECTING...
