8fe3e09277a84ebac16265f6d1780195a199586a
* <lucasduartedeveloper@gmail.com>
2022-07-03 13:40:59 -0300

    first commit

--- odometer/index.php
+++ odometer/index.php
@@ -54,7 +54,7 @@
 </div>
 
 <p id="version-info">
-     ODOMETER v30.0.46
+     ODOMETER v30.0.47
      <br>
      <span id="server-info">
      CONNECTING...
@@ -82,7 +82,7 @@
 <script src="script/matter.min.js"></script>
 
 <script src="script/websocket.js?v=0"></script>
-<script src="script/odometer.js?v=46"></script>
+<script src="script/odometer.js?v=47"></script>
 
 <script src="//cdn.jsdelivr.net/npm/eruda"></script>
 <script>eruda.init();</script>
