231a5cfe9dc493e58c50381cef084f62fae24168
* <lucasduartedeveloper@gmail.com>
2022-07-03 12:04:01 -0300

    first commit

--- odometer/index.php
+++ odometer/index.php
@@ -21,7 +21,7 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
 <!-- <link rel="stylesheet" href="css/normalizee.css"> -->
-<link rel="stylesheet" href="css/style.css?v=28">
+<link rel="stylesheet" href="css/style.css?v=29">
 
 <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
 
@@ -54,7 +54,7 @@
 </div>
 
 <p id="version-info">
-     ODOMETER v28.0.38
+     ODOMETER v29.0.39
      <br>
      <span id="server-info">
      CONNECTING...
@@ -82,7 +82,7 @@
 <script src="script/matter.min.js"></script>
 
 <script src="script/websocket.js?v=0"></script>
-<script src="script/odometer.js?v=38"></script>
+<script src="script/odometer.js?v=39"></script>
 
 <script src="//cdn.jsdelivr.net/npm/eruda"></script>
 <script>eruda.init();</script>
