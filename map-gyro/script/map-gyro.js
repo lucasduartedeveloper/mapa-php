d00172b8eec173fe13fbf0b14390418a717c5d83
* <lucasduartedeveloper@gmail.com>
2022-07-06 16:59:51 -0300

    first commit

--- map-gyro/script/map-gyro.js
+++ map-gyro/script/map-gyro.js
@@ -152,9 +152,11 @@ function accHandler(acc) {
 function calcularAngulo(co, ca, h) {
     var senA = co/h;
     var a = Math.asin(senA);
+    /*
     a = co == 0 && ca > 0 ? 1.5707963267948966 * 2 : a;
     a = co > 0 && ca > 0 ? 1.5707963267948966 * 2 - a : a;
     a = co < 0 && ca > 0 ? 1.5707963267948966 * 2 - a : a;
+    */
     return a * (180/Math.PI);
 }
 
