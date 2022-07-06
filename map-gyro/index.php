8fb6f5aed3d6f57c15f9e595512e10c8c591d4f2
* <lucasduartedeveloper@gmail.com>
2022-07-06 15:26:12 -0300

    first commit

--- map-gyro/index.php
+++ map-gyro/index.php
@@ -19,7 +19,7 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
 <!-- <link rel="stylesheet" href="css/normalizee.css"> -->
-<link rel="stylesheet" href="css/style.css?v=17">
+<link rel="stylesheet" href="css/style.css?v=18">
 
 <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
 
@@ -113,7 +113,7 @@ class="form-range">
 </button>
 
 <p id="version-info">
-     MAP-GYRO v17.0.4.33
+     MAP-GYRO v18.0.4.33
      <br>
      <span id="server-info">
      CONNECTING...
