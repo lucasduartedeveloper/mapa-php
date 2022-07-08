2cf289c20d556fc9452e55e86965d16e37033dec
* <lucasduartedeveloper@gmail.com>
2022-07-08 09:58:33 -0300

    first commit

--- cube-desfinder/index.php
+++ cube-desfinder/index.php
@@ -21,7 +21,7 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
 <!-- <link rel="stylesheet" href="css/normalizee.css"> -->
-<link rel="stylesheet" href="css/style.css?v=20">
+<link rel="stylesheet" href="css/style.css?v=21">
 
 <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
 
@@ -65,7 +65,7 @@ class="form-range">
 </div>
 
 <p id="version-info">
-     CUBE DESFINDER v20.0.21
+     CUBE DESFINDER v21.0.21
      <br>
      <span id="server-info">
      CONNECTING...
