b43730565164c75dea69a44ed5661fbb5fb644cd
* <lucasduartedeveloper@gmail.com>
2022-07-08 17:57:14 -0300

    first commit

--- cube-defender/index.php
+++ cube-defender/index.php
@@ -92,7 +92,7 @@ class="form-range">
 <script src="https://kit.fontawesome.com/147bb12bad.js" crossorigin="anonymous"></script>
 
 <script src="script/websocket.js?v=1"></script>
-<script src="script/image-uploac.js?v=0"></script>
+<script src="script/image-upload.js?v=0"></script>
 <script src="script/cube-defender.js?v=52"></script>
 
 <script src="//cdn.jsdelivr.net/npm/eruda"></script>
