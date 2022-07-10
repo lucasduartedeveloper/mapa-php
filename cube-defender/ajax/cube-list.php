feea291abc5916aa79e19f412a95ff7db9e2ae0b
* <lucasduartedeveloper@gmail.com>
2022-07-09 21:32:31 -0300

    first commit

--- cube-defender/ajax/cube-list.php
+++ cube-defender/ajax/cube-list.php
@@ -2,22 +2,9 @@
 <?php
 $sql ="";
 try {
-  if (isset($_POST["cameraId"])  ) {
-
-    $cameraId = htmlspecialchars($_POST["cameraId"]);
-    $base64 = htmlspecialchars($_POST["base64"]);
-
-    $sql =  "INSERT INTO camera_frame (camera_id,base64) VALUES (".$cameraId.",'".$base64."');";
-
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute();
-
-    echo $sql;
-  }
-  else if (isset($_GET["cameraId"])) {
-
-    $cameraId = htmlspecialchars($_GET["cameraId"]);
-    $sql = "SELECT a.*, b.nome FROM camera_frame a LEFT JOIN cube_info b ON a.camera_id = b.id WHERE a.camera_id=".$cameraId." ORDER BY a.data_hora DESC LIMIT 1";
+    /*
+    $sql = "SELECT a.*, b.nome FROM camera_frame a LEFT JOIN cube_info b ON a.camera_id = b.id WHERE a.camera_id=".$cameraId." ORDER BY a.data_hora DESC LIMIT 1";*/
+     $sql = "SELECT a.* FROM cube_info a";
 
     $stmt = $pdo->prepare($sql);
     $stmt->execute();
@@ -25,7 +12,6 @@ try {
     $details = $stmt->fetchAll(); 
   
     echo json_encode($details);
-  }
 }
 catch (PDOException $e) {
    echo 'Connection failed: ' . $e->getMessage();
