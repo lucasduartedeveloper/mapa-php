d85ad5b6fdf4133820a5f0ca5bfb916a0dc5079d
* <lucasduartedeveloper@gmail.com>
2022-07-09 22:54:35 -0300

    first commit

--- cube-defender/index.php
+++ cube-defender/index.php
@@ -90,7 +90,7 @@ class="form-range">
 </div>
 
 <p id="version-info">
-     CUBE DEFENDER v32.0.3.2.66
+     CUBE DEFENDER v32.0.3.2.67
      <br>
      <span id="server-info">
      CONNECTING...
@@ -143,7 +143,7 @@ class="form-range">
 <script src="script/gyro.js?v=0"></script>
 <script src="script/websocket.js?v=3"></script>
 <script src="script/image-upload.js?v=2"></script>
-<script src="script/cube-defender.js?v=66"></script>
+<script src="script/cube-defender.js?v=67"></script>
 
 <script src="//cdn.jsdelivr.net/npm/eruda"></script>
 <script>eruda.init();</script>
