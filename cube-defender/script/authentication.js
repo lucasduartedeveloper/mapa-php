2d1f94644207c810c1cf4f733837131cea992489
* <lucasduartedeveloper@gmail.com>
2022-07-11 01:20:33 -0300

    first commit

--- cube-defender/script/authentication.js
+++ cube-defender/script/authentication.js
@@ -3,8 +3,8 @@ var maxBrightness = 0;
 
 function authenticate(data) {
     for (var i = (data.length/2); i <= (data.length/2); i += 4) {
-        /*var brightness = 0.34 * data[i] + 
-        0.5 * data[i + 1] + 0.16 * data[i + 2];*/
+        var brightness = 0.34 * data[i] + 
+        0.5 * data[i + 1] + 0.16 * data[i + 2];
         // red
         //data[i] = brightness;
         //lightData[i] = 
@@ -23,18 +23,16 @@ function authenticate(data) {
         data[i] + "," + 
         data[i + 1]  + "," +
         data[i + 2] + 
-         ")");
-        
+         ")");        
+
         if (brightness > maxBrightness) {
-            maxBrightness = brightness;
+            //maxBrightness = brightness;
             //light.x = Math.floor(i/4) % 100;
             //light.y = Math.floor(i/4) / 100;
         }
     }
 
-    authenticated =
-    $("#color-code").text() ==
-    "rgb(255,255,255)";
+    authenticated = maxBrightness > 250;
     if (authenticated) {
         //notification.play();
         $("#authentication").hide();
