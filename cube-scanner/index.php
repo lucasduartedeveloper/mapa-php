5eb60c8c8fd698c73850934cd1059029a8a61de9
* <lucasduartedeveloper@gmail.com>
2022-07-12 15:30:07 -0300

    first commit

--- cube-scanner/index.php
+++ cube-scanner/index.php
@@ -120,7 +120,7 @@ class="form-range">
 </div>
 
 <p id="version-info">
-     CUBE SCANNER v47.14-30.2.200
+     CUBE SCANNER v47.14-30.2.201
      <br>
      <span id="server-info">
      CONNECTING...
@@ -194,7 +194,7 @@ class="form-range">
 
 <script src="script/cube-shadow.js?v=30"></script>
 <script src="script/image-upload.js?v=2"></script>
-<script src="script/cube-scanner.js?v=200"></script>
+<script src="script/cube-scanner.js?v=201"></script>
 
 <script src="//cdn.jsdelivr.net/npm/eruda"></script>
 <script>eruda.init();</script>
