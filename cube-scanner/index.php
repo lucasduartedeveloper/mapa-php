c279ee7f6000c9d14e040c6746c762fa633caac4
* <lucasduartedeveloper@gmail.com>
2022-07-12 13:59:28 -0300

    first commit

--- cube-scanner/index.php
+++ cube-scanner/index.php
@@ -25,7 +25,7 @@
 <link rel="stylesheet" 
          href="css/style.css?v=47">
 <link rel="stylesheet" 
-         href="css/anim/loading.css?v=3">
+         href="css/anim/loading.css?v=4">
 
 <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
 
@@ -121,7 +121,7 @@ class="form-range">
 </div>
 
 <p id="version-info">
-     CUBE SCANNER v47.3 - 30.2.194
+     CUBE SCANNER v47.4 - 30.2.194
      <br>
      <span id="server-info">
      CONNECTING...
