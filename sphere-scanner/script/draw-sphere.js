b8afce6fb18223a6f2d02d59476e283a782b9726
* <lucasduartedeveloper@gmail.com>
2022-07-13 19:32:21 -0300

    first commit

--- sphere-scanner/script/draw-sphere.js
+++ sphere-scanner/script/draw-sphere.js
@@ -23,8 +23,8 @@ function angle(co, ca) {
     var a = Math.asin(senA);
     a = co == 0 && ca > 0 ?
     1.5707963267948966 * 2 : a;
-    a = co > 0 && ca > 0 ?
-    1.5707963267948966 * 2 - a : a;
+    /*a = co > 0 && ca > 0 ?
+    1.5707963267948966 * 2 - a : a;*/
     a = co < 0 && ca > 0 ?
     1.5707963267948966 * 2 - a : a;
     return a * (180/Math.PI);
