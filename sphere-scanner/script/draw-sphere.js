391c7d9b410dd6e5dee57dc899f3d652efd382f3
* <lucasduartedeveloper@gmail.com>
2022-07-13 03:54:01 -0300

    first commit

--- sphere-scanner/script/draw-sphere.js
+++ sphere-scanner/script/draw-sphere.js
@@ -6,7 +6,13 @@ function drawSphere() {
                 matrix[k] = [];
                 matrix[k][n] = [0,0,64];
                 rotateNode3DonX(matrix[k][n], k*36);
-
+                for (var o = 0; o < 10; o++) {
+                     matrix[o][n] = matrix[k][n];
+                     rotateNode3DonY(matrix[o][n], k*36);
+                }
+          }
+          
+          for (var n = 0; n < 10; n++) {
                 var img = new Image();
                 img.width = 128 / 10;
                 img.height = 128 / 10;
@@ -22,9 +28,10 @@ function drawSphere() {
                 "-webkit-transform-style" :
                 "preserve-3d",
                 "transform" :
-                "translateZ("+matrix[k][n][2]+"px) "+
+                "translateX("+matrix[k][n][0]+"px) "+
                 "translateY("+matrix[k][n][1]+"px) "+
-                "rotateY("+(k * 36)+"deg)"
+                "translateZ("+matrix[k][n][2]+"px) "+
+                "rotateX("+(k * 36)+"deg)"
                 });
           }
      }
