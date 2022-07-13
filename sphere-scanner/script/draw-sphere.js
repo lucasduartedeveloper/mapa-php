d0066b8e2e87f27a10f918814d067db5a77b6034
* <lucasduartedeveloper@gmail.com>
2022-07-13 13:43:03 -0300

    first commit

--- sphere-scanner/script/draw-sphere.js
+++ sphere-scanner/script/draw-sphere.js
@@ -9,8 +9,8 @@ function drawSphere() {
            matrix[n] = new Array(10);
            for (var o = 0; o < 10; o++) {
                 matrix[n][o] = [0,0,64];
-                rotateNode3DonX(matrix[n][o], (n*18));
-                rotateNode3DonY(matrix[n][o], (o*18)*-1);
+                rotateNode3DonX(matrix[n][o], (n*18)*-1);
+                rotateNode3DonY(matrix[n][o], (o*18));
            }
        }
       
