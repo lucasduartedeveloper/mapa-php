235fa4d0a0622c815825a7723df73719b57366da
* <lucasduartedeveloper@gmail.com>
2022-07-13 18:44:23 -0300

    first commit

--- sphere-scanner/script/draw-sphere.js
+++ sphere-scanner/script/draw-sphere.js
@@ -1,4 +1,4 @@
-var matrix = new Array(20);
+var matrix = new Array(10);
 
 // Criar um anel vertical
 // Duplicar várias vezes
@@ -25,17 +25,17 @@ function angle(co, ca) {
 }
 
 function drawSphere() {
-      for (var n = 0; n < 2; n++) {
-           matrix[n] = new Array(20);
-           for (var o = 0; o < 20; o++) {
+      for (var n = 0; n < 3; n++) {
+           matrix[n] = new Array(10);
+           for (var o = 0; o < 10; o++) {
                 matrix[n][o] = [0,0,64];
                 rotateNode3DonX(matrix[n][o], (n*18));
                 rotateNode3DonY(matrix[n][o], (o*18));
            }
        }
       
-      for (var k = 0; k < 2; k++) {
-          for (var n = 0; n < 20; n++) {
+      for (var k = 0; k < 3; k++) {
+          for (var n = 0; n < 10; n++) {
                 var img = new Image();
                 img.width = ((128*Math.PI)/2) / 10;
                 img.height = ((128*Math.PI)/2) / 10;
