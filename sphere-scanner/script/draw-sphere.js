ca5bab972ea67db9a84f0c70fadcf193d171fb0d
* <lucasduartedeveloper@gmail.com>
2022-07-13 21:07:01 -0300

    first commit

--- sphere-scanner/script/draw-sphere.js
+++ sphere-scanner/script/draw-sphere.js
@@ -63,8 +63,8 @@ function drawSphere() {
                 "-webkit-transform-style" :
                 "preserve-3d",
                 "transform" :
-                "rotateX("+(n*((360/8)*-1))+"deg) "+
-                "rotateY("+(k*((360/8)))+"deg) " +
+                "rotateX("+(k*((360/8)))+"deg) "+
+                "rotateY("+(n*((360/8)))+"deg) " +
                 /*"rotateZ("+(rz)+"deg)"*/
                 "translateX("+(tx)+"px) "+
                 "translateY("+(ty)+"px) "+
