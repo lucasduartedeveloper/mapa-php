85f5b6446f8b7988b26f0d451c386d6118466e6c
* <lucasduartedeveloper@gmail.com>
2022-07-15 05:09:13 -0300

    first commit

--- cube-scanner/script/cube-scanner.js
+++ cube-scanner/script/cube-scanner.js
@@ -313,9 +313,9 @@ $(document).ready(function() {
                  .getElementById("interface")
                  .getContext("2d")
                  .drawImage(video, 
-                 (((sw/vr)-sw)/2)*-1, 
+                 (((sw-vw)/2)/2)*-1, 
                  ((sh-vh)/2)*-1, 
-                 sw/vr, vh);
+                 vw, vh);
              }
              addShadow();
          }
