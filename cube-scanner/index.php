6dca6d76ff643929aca3762ee709656054263586
* <lucasduartedeveloper@gmail.com>
2022-07-15 04:00:36 -0300

    first commit

--- cube-scanner/index.php
+++ cube-scanner/index.php
@@ -23,7 +23,7 @@
 <!-- <link rel="stylesheet" href="css/normalizee.css"> -->
 
 <link rel="stylesheet" 
-         href="css/style.css?v=60">
+         href="css/style.css?v=61">
 <link rel="stylesheet" 
          href="css/anim/loading.css?v=22">
 
@@ -139,7 +139,7 @@ class="form-range">
 </div>
 
 <p id="version-info">
-     CUBE SCANNER v60.14-30.2.265
+     CUBE SCANNER v61.14-30.2.265
      <br>
      <span id="server-info">
      CONNECTING...
