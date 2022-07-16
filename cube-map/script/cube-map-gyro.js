3eaa56787038771b86d2ad875f24fe7b6d1cd12f
* <lucasduartedeveloper@gmail.com>
2022-07-16 19:22:58 -0300

    first commit

--- cube-map/script/cube-map-gyro.js
+++ cube-map/script/cube-map-gyro.js
@@ -370,6 +370,15 @@ setInterval(function() {
          "rotateZ("+ (cubeRotateZ) + "deg)");
 }, 100);
 
+var baseImages = [
+      "cube-scanner/img/front.png",
+      "cube-scanner/img/back.png",
+      "cube-scanner/img/left.png",
+      "cube-scanner/img/top.png",
+      "cube-scanner/img/right.png",
+      "cube-scanner/img/bottom.png"
+];
+
 var gotXYZ = false;
 function getXYZ(callback) {
      $.getJSON(
