73d2774305419be6f983510e621e8359c4db0f66
* <lucasduartedeveloper@gmail.com>
2022-07-19 05:29:12 -0300

    first commit

--- cube-scanner/print.php
+++ cube-scanner/print.php
@@ -4,7 +4,7 @@
 
 <meta content='width=device-width, initial-scale=0.5, maximum-scale=1.0, user-scalable=0' name='viewport' />
 
-<link rel="stylesheet" href="css/print-cube.css?v9">
+<link rel="stylesheet" href="css/print-cube.css?v10">
 <title></title>
 </head>
 <body>
