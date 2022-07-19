f3bb5b5c5adf9c6b8e7c4a7293d381e7107396af
* <lucasduartedeveloper@gmail.com>
2022-07-19 05:02:02 -0300

    first commit

---
+++ cube-scanner/script/print-cube.js
@@ -0,0 +1,26 @@
+var queryString = window.location.search;
+var urlParams = new URLSearchParams(queryString);
+var cubeId = urlParams.get("cubeId");
+
+$(document).ready(function() {
+    loadCubeToPrint(cubeId);
+});
+
+function loadCubeToPrint(id) {
+    $.getJSON("ajax/cube-face.php?cubeId="+id, 
+    function(data) {
+
+         var faces = $("#cube-container img");
+         for (var k = 0; k < 6; k++) {
+              var c = data.filter(o => o.face_id == k);
+
+              faces[k].src = c.length == 0 ? 
+              baseImages[k] : 
+              ( c[0].base64 ? c[0].base64 : 
+              baseImages[k]); // *
+         }
+         //setCubeInfo();
+
+        log("get", data);
+    });
+}
