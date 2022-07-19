1ef44ab9406757f9431b2ffa3a18d037b51b0c99
* <lucasduartedeveloper@gmail.com>
2022-07-19 04:09:16 -0300

    first commit

--- cube-scanner/print.php
+++ cube-scanner/print.php
@@ -2,13 +2,30 @@
 <html>
 <head>
 
-<link rel="stylesheet" href="css/print-cube.css?v=0">
+<link rel="stylesheet" href="css/print-cube.css?v=1">
 <title></title>
 </head>
 <body>
 
+<div id="cube-container">
+      <img face-id="0"
+      src="img/front.png" class="front"/> 
+      <img face-id="1"
+      src="img/back.png" class="back"/>
+      <img face-id="2"
+      src="img/left.png" class="left"/>
+      <img face-id="3"
+      src="img/top.png" class="top"/>
+      <img face-id="4"
+      src="img/right.png" class="right"/>
+      <img face-id="5"
+      src="img/bottom.png" class="bottom"/>
+      <img face-id="7"
+      src="img/inside.jpg" class="inside"/>
+</div>
+
 <p id="version-info">
-     CUBE SCANNER v63.14-30.2.290
+     CUBE SCANNER v###
 </p>
 
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
