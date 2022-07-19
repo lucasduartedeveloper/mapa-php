617cead79288d6e6b633c9eb89e7439d40488b0e
* <lucasduartedeveloper@gmail.com>
2022-07-19 19:45:54 -0300

    first commit

--- cube-scanner/index.php
+++ cube-scanner/index.php
@@ -240,8 +240,7 @@ class="form-range">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 id="cube-modal-title" 
-        class="modal-title">BUGFIX</h5>
+        <h5  class="modal-title">BUGFIX</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
