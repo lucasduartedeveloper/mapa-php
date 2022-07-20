51242c88cebe7e329292d149954b6f5818d5c2e3
* <lucasduartedeveloper@gmail.com>
2022-07-20 16:05:58 -0300

    first commit

---
+++ 3d/londrina/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300&family=Roboto+Slab:wght@200&display=swap" rel="stylesheet">
+
+<link rel="apple-touch-icon" sizes="76x76" href="webapp/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="webapp/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="webapp/favicon-16x16.png">
+<link rel="manifest" href="webapp/site.webmanifest?v=0">
+<link rel="mask-icon" href="webapp/safari-pinned-tab.svg" color="#2f2e40">
+<meta name="msapplication-TileColor" content="#2f2e40">
+<meta name="theme-color" content="#2f2e40">
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+
+<link rel="stylesheet" href="css/index.css?v=0">
+
+<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+
+<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+
+<title></title>
+</head>
+<body>
+
+<div id="app">
+</div>
+
+<p id="version-info">
+     <span>
+     v0
+     </span>
+     <br>
+     <span id="server-info">
+     CONNECTING...
+     </span>
+</p>
+
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js"></script>
+
+<script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+
+<script src="https://momentjs.com/downloads/moment.min.js"></script>
+
+<script src="https://kit.fontawesome.com/147bb12bad.js" crossorigin="anonymous"></script>
+
+<script src="script/debug.js?v=0"></script>
+<script src="script/app.js?v=0"></script>
+
+<script src="//cdn.jsdelivr.net/npm/eruda"></script>
+<script>
+    eruda.init();
+
+    if (location.protocol !== "https:") {
+        location.replace(`https:${location.href.substring(location.protocol.length)}`);
+    }
+</script>
+</body>
+</html> 
