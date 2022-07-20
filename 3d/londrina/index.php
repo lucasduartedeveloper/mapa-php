13145ff30fde2643460bd7d0077024ad6fa939f3
* <lucasduartedeveloper@gmail.com>
2022-07-20 16:51:30 -0300

    first commit

--- 3d/londrina/index.php
+++ 3d/londrina/index.php
@@ -29,7 +29,8 @@
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
-<link rel="stylesheet" href="css/index.css?v=0">
+<link rel="stylesheet" 
+href="css/index.css?rnd=<? echo $rnd; ?>">
 
 <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
 
