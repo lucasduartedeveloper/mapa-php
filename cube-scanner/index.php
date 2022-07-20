6bb0da97f4bd810e2f1779251b656ab2fdf888c8
* <lucasduartedeveloper@gmail.com>
2022-07-19 23:07:22 -0300

    first commit

--- cube-scanner/index.php
+++ cube-scanner/index.php
@@ -283,7 +283,7 @@ class="form-range">
 <script src="https://kit.fontawesome.com/147bb12bad.js" crossorigin="anonymous"></script>
 
 <script src="script/debug.js?v=0"></script>
-<script src="script/gyro.js?v=11"></script>
+<script src="script/gyro.js?v=12"></script>
 
 <script src="script/coloris.min.js"></script>
 
