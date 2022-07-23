d42c4f09fd8d62a0f8594a94c7c28665c57823ad
* <lucasduartedeveloper@gmail.com>
2022-07-22 23:42:06 -0300

    first commit

--- gyro-audio/script/app.js
+++ gyro-audio/script/app.js
@@ -1,6 +1,8 @@
-var audio = new Audio("
-https:\/\/ice.fabricahost.com.br\/jovempanlondrina");
+var audio = new Audio(
+"https:\/\/ice.fabricahost.com.br\/jovempanlondrina");
 
 $(document).ready(function() {
     audio.play();
+    $("#volume").text("VOLUME: "+
+    gyro.accY/9.8);
 });
