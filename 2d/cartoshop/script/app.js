a199e6124224d810db3efedc9fcc16cc76f36cc7
* <lucasduartedeveloper@gmail.com>
2022-07-26 20:50:37 -0300

    first commit

--- 2d/cartoshop/script/app.js
+++ 2d/cartoshop/script/app.js
@@ -84,7 +84,7 @@ $(document).ready(function() {
         }
 
         if (accelerating) {
-            fuel -= 0.1;
+            fuel -= 1;
             fuel = fuel < 0 ? 0 : fuel;
             $("#fuel-ammount").css("width", (fuel/2)+"vw");
         }
