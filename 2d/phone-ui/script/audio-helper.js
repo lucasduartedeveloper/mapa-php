5ba3e34f3fd89d6329b3ac2f2159dbcff538efea
* <lucasduartedeveloper@gmail.com>
2022-07-27 05:39:31 -0300

    first commit

--- 2d/phone-ui/script/audio-helper.js
+++ 2d/phone-ui/script/audio-helper.js
@@ -1,12 +1,12 @@
 var dial = [
-    new Audio("audio/phone-dial-0.wav");
-    new Audio("audio/phone-dial-1.mp3");
-    new Audio("audio/phone-dial-2.mp3");
-    new Audio("audio/phone-dial-3.mp3");
-    new Audio("audio/phone-dial-4.mp3");
-    new Audio("audio/phone-dial-5.mp3");
-    new Audio("audio/phone-dial-6.mp3");
-    new Audio("audio/phone-dial-7.mp3");
+    new Audio("audio/phone-dial-0.wav"),
+    new Audio("audio/phone-dial-1.mp3"),
+    new Audio("audio/phone-dial-2.mp3"),
+    new Audio("audio/phone-dial-3.mp3"),
+    new Audio("audio/phone-dial-4.mp3"),
+    new Audio("audio/phone-dial-5.mp3"),
+    new Audio("audio/phone-dial-6.mp3"),
+    new Audio("audio/phone-dial-7.mp3")
 ];
 
 function playRandomDialSound() {
