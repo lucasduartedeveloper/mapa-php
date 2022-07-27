837f33a3b6daf66de82d72135500eb2ebcd512e0
* <lucasduartedeveloper@gmail.com>
2022-07-27 17:23:42 -0300

    first commit

--- 2d/calendar/script/2d-objects.js
+++ 2d/calendar/script/2d-objects.js
@@ -166,6 +166,9 @@ Matter.Body.setCentre(secondCircle,
 Matter.Body.setCentre(minuteCircle, 
    { x: 0, y: -(sw/2) }, true);
 
+Matter.Body.setAngularVelocity(secondCircle, 10);
+Matter.Body.setAngularVelocity(minuteCircle, 10);
+
 var mouse = Matter.Mouse.create(render.canvas);
 var mouseConstraint = 
 Matter.MouseConstraint.create(engine, {
