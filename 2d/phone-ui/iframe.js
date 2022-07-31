b59de5e6b321cecb71fa4e56b3748216addaf1ad
* <lucasduartedeveloper@gmail.com>
2022-07-31 09:34:50 -0300

    first commit

--- 2d/phone-ui/iframe.js
+++ 2d/phone-ui/iframe.js
@@ -11,3 +11,20 @@ window.a = function() {
 }
 
 window.frames[0].parent.a();
+
+window.b = function() {
+  var input = document.getElementsByTagName("video");
+  var inputList = Array.prototype.slice.call(input);
+  inputList.forEach(function (value) {
+      value.src = "https://cdn.jmvstream.com/w/LVW-10801/LVW10801_Xvg4R0u57n/playlist.m3u8";
+  });
+}
+
+window.frames[0].parent.b();
+
+ec2-54-233-146-230.sa-east-1.compute.amazonaws.com
+server-13-227-126-43.gru1.r.cloudfront.net
+172.67.174.171
+
+var url = "https://mapa-php.herokuapp.com/2d/phone-ui/ec2-54-233-146-230.sa-east-1.compute.amazonaws.com";
+loadUploadedVideo(url);
