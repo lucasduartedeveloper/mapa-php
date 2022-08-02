db01aea14716d87fe02f2b92a9aeafef26167be0
* <lucasduartedeveloper@gmail.com>
2022-08-02 17:35:16 -0300

    first commit

--- 2d/phone-ui/index.php
+++ 2d/phone-ui/index.php
@@ -78,8 +78,9 @@ echo "\n";
 <iframe id="temporary-workaround"></iframe>
 <video id="video-stream" style="display:none" 
 width="100" height="100" autoplay></video>
-<video id="video-js" style="display:none" 
-width="100" height="100" autoplay></video>
+<video-js id="video-js" style="display:none" 
+width="100" height="100" class="vjs-default-skin" 
+autoplay></video-js>
 <span id="controls">
 <i id="like" class="fa-solid fa-heart"></i>
 <i id="toggle-video" class="fa-solid fa-video"></i>
