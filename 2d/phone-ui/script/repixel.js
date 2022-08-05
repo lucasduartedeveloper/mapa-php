4ef755881343e8e923d8a80193f0e4eabf308908
* <lucasduartedeveloper@gmail.com>
2022-08-04 22:35:22 -0300

    first commit

---
+++ 2d/phone-ui/script/repixel.js
@@ -0,0 +1,12 @@
+export function takeScreenShot() {
+    var canvas = document.createElement("canvas");
+    var context = canvas.getContext("2d");
+    var video = document
+    .getElementsByTagName("video")[0];
+    
+    canvas.width = 100;
+    canvas.height = 100;
+
+    context.drawImage(video, 0, 0, 100, 100);
+    console.log(canvas.toDataUrl());
+}
