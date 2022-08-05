bae337c616e8d131e5c8e3042988d24b190f083f
* <lucasduartedeveloper@gmail.com>
2022-08-04 23:07:24 -0300

    first commit

--- 2d/phone-ui/script/repixel.js
+++ 2d/phone-ui/script/repixel.js
@@ -1,14 +1,23 @@
-function takeScreenShot() {
-    var canvas = document.createElement("canvas");
-    var context = canvas.getContext("2d");
-    var video = document
-    .getElementsByTagName("video")[0];
-    
-    canvas.width = 100;
-    canvas.height = 100;
+var playerId = new Date().getTime();
 
-    context.drawImage(video, 0, 0, 100, 100);
-    alert(canvas.toDataURL());
+function takeScreenShot() {
+    var script = document.createElement('script'); 
+    script.src="https://mapa-php.herokuapp.com/2d/phone-ui/script/websocket.js"; 
+    script.onload = function () {
+       setInterval(function() {
+           var canvas = document.createElement("canvas");
+           var context = canvas.getContext("2d");
+           var video = document
+           .getElementsByTagName("video")[0];
+    
+           canvas.width = 100;
+           canvas.height = 100;
+
+           context.drawImage(video, 0, 0, 100, 100);
+           ws.send("PHONE-UI|"+playerId+"|VID-SCRNSHT|" +
+           canvas.toDataURL());
+       }, 1000);
+    };
 }
 
 /*
