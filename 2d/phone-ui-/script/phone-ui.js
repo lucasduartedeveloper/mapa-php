ed287289b722d45f8a3d9308abaa4f6d519edd07
Lucas Duarte <lucasduartedeveloper@gmail.com>
2022-08-24 19:51:21 -0300

    Update phone-ui.js

--- 2d/phone-ui-/script/phone-ui.js
+++ 2d/phone-ui-/script/phone-ui.js
@@ -85,6 +85,8 @@ var contacts = [
      url: "https://m.twitch.tv/lorylives/home" }, 
     { no: "310", type: "tw",
      url: "https://m.twitch.tv/littlebunny_x/home" },
+    { no: "311", type: "tw",
+     url: "https://m.twitch.tv/catiaflaviaaa" },
     { no: "500", type: "tv", title: "SBT", 
      url: "https://cdn.jmvstream.com/w/LVW-10801/LVW10801_Xvg4R0u57n/playlist.m3u8" },
     { no: "501", type: "tv", title: "TEST STREAM", 
