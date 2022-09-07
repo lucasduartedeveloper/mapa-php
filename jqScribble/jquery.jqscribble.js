46d9c9081907f44a26d9cde8ce4421a665753242
Lucas Duarte <lucasduartedeveloper@gmail.com>
2022-09-07 15:16:43 -0300

    Update jquery.jqscribble.js

--- jqScribble/jquery.jqscribble.js
+++ jqScribble/jquery.jqscribble.js
@@ -204,6 +204,9 @@ function BasicCanvasSave(imageData){window.open(imageData,'jqScribble Image');}
                 },
                 mousemove: function(e)
                 {
+                    // Rotation
+                    console.log("x: " + e.pageX, "y: " + e.pageY);
+                    //
                     var o = $elm.offset();
                     if(self.brush.active)self.brush.strokeMove(e.pageX-o.left, e.pageY-o.top);
                 },
